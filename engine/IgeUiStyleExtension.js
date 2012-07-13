8549e13e75996997d149d7604a00b7cfc5e32259
Rob Evans <coolbloke1324@gmail.com>
2012-07-13 04:03:17 +0100

    Altered backgroundImage to accept a single repeatType argument instead of repeatX and repeatY flags

--- engine/IgeUiStyleExtension.js
+++ engine/IgeUiStyleExtension.js
@@ -1,31 +1,68 @@
 var IgeUiStyleExtension = {
 	/**
-	 * Sets the current background texture and the repeatX
-	 * and repeatY flags to determine in which axis the image
-	 * should be repeated.
+	 * Sets the current background texture and the repeatType
+	 * to determine in which axis the image should be repeated.
+	 * Accepts "repeat", "repeat-x", "repeat-y" and "no-repeat".
 	 * @param {IgeTexture=} texture
-	 * @param {Boolean=} repeatX
-	 * @param {Boolean=} repeatY
+	 * @param {String=} repeatType
 	 * @return {*} Returns this if any parameter is specified or
-	 * the current IgeTexture if no parameters are specified.
+	 * the current background image if no parameters are specified.
 	 */
-	backgroundImage: function (texture, repeatX, repeatY) {
-		if (texture !== undefined) {
-			this.texture(texture);
+	backgroundImage: function (texture, repeatType) {
+		if (texture && texture.image) {
+			if (!repeatType) { repeatType = 'no-repeat'; }
+
+			// Store the repeatType
+			this._patternRepeat = repeatType;
+
+			// Store the texture
+			this._patternTexture = texture;
+
+			// Resize the image if required
+			if (this._backgroundSize) {
+				texture.resize(this._backgroundSize.x, this._backgroundSize.y);
+				this._patternWidth = this._backgroundSize.x;
+				this._patternHeight = this._backgroundSize.y;
+			} else {
+				this._patternWidth = texture.image.width;
+				this._patternHeight = texture.image.height;
+			}
+
+			// Create the pattern from the texture
+			this._patternFill = ige._ctx.createPattern(texture.image, repeatType);
+
+			texture.restoreOriginal();
+			this.dirty(true);
 			return this;
 		}
 
-		if (repeatX !== undefined) {
-			this._backgroundRepeatX = repeatX;
+		return this._patternFill;
+	},
+
+	backgroundSize: function (x, y) {
+		if (x !== undefined && y !== undefined) {
+			if (typeof(x) === 'string') {
+				// Work out the actual size in pixels
+				// from the percentage
+				x = this._width / 100 * parseInt(x, 10);
+			}
+
+			if (typeof(y) === 'string') {
+				// Work out the actual size in pixels
+				// from the percentage
+				y = this._height / 100 * parseInt(y, 10);
+			}
+			this._backgroundSize = {x: x, y: y};
+
+			// Reset the background image
+			if (this._patternTexture && this._patternRepeat) {
+				this.backgroundImage(this._patternTexture, this._patternRepeat);
+			}
+			this.dirty(true);
 			return this;
 		}
 
-		if (repeatY !== undefined) {
-			this._backgroundRepeatY = repeatY;
-			return this;
-		}
-
-		return this._texture;
+		return this._backgroundSize;
 	},
 
 	/**
@@ -37,6 +74,7 @@ var IgeUiStyleExtension = {
 	backgroundColor: function (color) {
 		if (color !== undefined) {
 			this._backgroundColor = color;
+			this.dirty(true);
 			return this;
 		}
 
@@ -51,7 +89,8 @@ var IgeUiStyleExtension = {
 	 */
 	backgroundPosition: function (x, y) {
 		if (x !== undefined && y !== undefined) {
-			this._backgroundPosition = new IgePoint(x, z, 0);
+			this._backgroundPosition = {x: x, y: y};
+			this.dirty(true);
 			return this;
 		}
 
@@ -61,6 +100,11 @@ var IgeUiStyleExtension = {
 	borderColor: function (color) {
 		if (color !== undefined) {
 			this._borderColor = color;
+			this._borderLeftColor = color;
+			this._borderTopColor = color;
+			this._borderRightColor = color;
+			this._borderBottomColor = color;
+			this.dirty(true);
 			return this;
 		}
 
@@ -74,6 +118,7 @@ var IgeUiStyleExtension = {
 			this._borderTopWidth = px;
 			this._borderRightWidth = px;
 			this._borderBottomWidth = px;
+			this.dirty(true);
 			return this;
 		}
 
@@ -87,6 +132,7 @@ var IgeUiStyleExtension = {
 			this._borderTopRightRadius = px;
 			this._borderBottomRightRadius = px;
 			this._borderBottomLeftRadius = px;
+			this.dirty(true);
 			return this;
 		}
 
