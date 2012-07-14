b43180e7219ba1df582f47f5dd084b12e38dc347
Rob Evans <coolbloke1324@gmail.com>
2012-07-14 04:07:31 +0100

    Reverted server.js to standard server.js and made new test 6-server from the previous version

--- tests/2-viewports/server.js
+++ tests/2-viewports/server.js
@@ -7,23 +7,12 @@ var Server = IgeClass.extend({
 		ige.addComponent(IgeSocketIoComponent);
 		ige.network.start();
 
-		var texture1 = new IgeTexture('../assets/textures/sprites/fairy.png');
-
 		// Start the game engine
 		ige.start(function (success) {
 			// Check if the engine started successfully
 			if (success) {
 				// Accept incoming connections
 				ige.network.acceptConnections(true);
-
-				var scene1 = new IgeScene2d();
-				var vp1 = new IgeViewport()
-					.scene(scene1)
-					.mount(ige);
-
-				var entity1 = new IgeEntity()
-					.texture(texture1)
-					.mount(scene1);
 			}
 		});
 	}
