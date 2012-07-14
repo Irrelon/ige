1bdcfa8f2fbc46539dbe3cc1373610148b8374ea
Rob Evans <coolbloke1324@gmail.com>
2012-07-14 03:24:37 +0100

    Added methods to handle mouse interaction events

--- engine/extensions/IgeUiInteractionExtension.js
+++ engine/extensions/IgeUiInteractionExtension.js
@@ -8,6 +8,25 @@ var IgeUiInteractionExtension = {
 		return this._mouseMove;
 	},
 
+	_handleMouseIn: function () {
+		// TODO: Set local mouse point based on 0, 0 at top-left of entity rather than screen
+		// Check if the mouse move is a mouse over
+		if (!this._mouseStateOver) {
+			this._mouseStateOver = true;
+			if (this._mouseOver) { this._mouseOver.call(this); }
+		}
+
+		if (this._mouseMove) { this._mouseMove.call(this); }
+	},
+
+	_handleMouseOut: function () {
+		// Check if the mouse move is a mouse out
+		if (this._mouseStateOver) {
+			this._mouseStateOver = false;
+			if (this._mouseOut) { this._mouseOut.call(this); }
+		}
+	},
+
 	mouseOver: function (callback) {
 		if (callback) {
 			this._mouseOver = callback;
@@ -35,6 +54,10 @@ var IgeUiInteractionExtension = {
 		return this._mouseUp;
 	},
 
+	_handleMouseUp: function () {
+
+	},
+
 	mouseDown: function (callback) {
 		if (callback) {
 			this._mouseDown = callback;
@@ -42,6 +65,10 @@ var IgeUiInteractionExtension = {
 		}
 
 		return this._mouseDown;
+	},
+
+	_handleMouseDown: function () {
+
 	}
 };
 
