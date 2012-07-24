0a8ad54cfd0a09093e49d6604f8d367d2c69f5f0
Rob Evans <rob@irrelon.com>
2012-07-24 21:32:20 +0100

    Updated to use the new mousePos() method

--- engine/IgeInteractiveEntity.js
+++ engine/IgeInteractiveEntity.js
@@ -5,29 +5,37 @@ var IgeInteractiveEntity = IgeEntity.extend([
 	 * Processes the actions required each render frame.
 	 */
 	tick: function (ctx, dontTransform) {
-		var	aabb = this.aabb(),
-			mouseX = ige._mousePos.x,
-			mouseY = ige._mousePos.y;
+		this._mousePos = this.mousePos();
 
-		// Check if the current mouse position is inside this aabb
-		if (aabb && (aabb.x <= mouseX && aabb.y <= mouseY && aabb.x + aabb.width > mouseX && aabb.y + aabb.height > mouseY)) {
-			// Point is inside the aabb
-			if (ige.input.mouseMove) {
-				this._handleMouseIn();
-			}
+		if (this._mousePos) {
+			var	aabb = this.aabb(),
+				mouseX = this._mousePos.x,
+				mouseY = this._mousePos.y;
 
-			if (ige.input.mouseDown) {
-				// The mouse is down
-				this._handleMouseDown();
-			}
+			// Check if the current mouse position is inside this aabb
+			if (aabb && (aabb.x <= mouseX && aabb.y <= mouseY && aabb.x + aabb.width > mouseX && aabb.y + aabb.height > mouseY)) {
+				// Point is inside the aabb
+				if (ige.input.mouseMove) {
+					this._handleMouseIn();
+				}
 
-			if (ige.input.mouseUp) {
-				// The mouse went up
-				this._handleMouseUp();
-			}
-		} else {
-			if (ige.input.mouseMove) {
-				this._handleMouseOut();
+				if (ige.input.mouseDown) {
+					// The mouse is down
+					this._handleMouseDown();
+				}
+
+				if (ige.input.mouseUp) {
+					// The mouse went up
+					this._handleMouseUp();
+				}
+
+				/*ctx.fillStyle = '#ffffff';
+				ctx.fillText('Entity M (' + this._mousePos.x + ', ' + this._mousePos.y + ')', 0, 0);
+				ctx.fillText('Entity P (' + aabb.x + ', ' + aabb.y + ')', 0, 15);*/
+			} else {
+				if (ige.input.mouseMove) {
+					this._handleMouseOut();
+				}
 			}
 		}
 
