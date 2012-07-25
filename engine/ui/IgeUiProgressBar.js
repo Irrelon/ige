5d5fcba4d8a06e675088f1291e54ddeb3ce49829
Rob Evans <rob@irrelon.com>
2012-07-25 18:47:00 +0100

    Updated to work with 1.1.0

--- engine/ui/IgeUiProgressBar.js
+++ engine/ui/IgeUiProgressBar.js
@@ -1,47 +1,136 @@
-var image = {
-	render: function (ctx, entity) {
-		if (entity.ui && entity.ui.progress) {
-			var progress = entity.ui.progress;
-			
+// TODO: Document this class
+var IgeUiProgressBar = IgeUiEntity.extend({
+	init: function () {
+		this._super();
+
+		// Set some defaults
+		this._min = 0;
+		this._max = 100;
+		this._progress = 0;
+		this._barBackColor = '#000000';
+		this._barColor = '#fff600';
+		this._barBorderColor = '#ffffff';
+		this._barText = {
+			pre: '',
+			post: '',
+			color: ''
+		};
+	},
+
+	barBackColor: function (val) {
+		if (val !== undefined) {
+			this._barBackColor = val;
+			return this;
+		}
+
+		return this._barBackColor;
+	},
+
+	barColor: function (val) {
+		if (val !== undefined) {
+			this._barColor = val;
+			return this;
+		}
+
+		return this._barColor;
+	},
+
+	barBorderColor: function (val) {
+		if (val !== undefined) {
+			this._barBorderColor = val;
+			return this;
+		}
+
+		return this._barBorderColor;
+	},
+
+	barText: function (pre, post, color) {
+		if (pre !== undefined && post !== undefined && color !== undefined) {
+			this._barText = {
+				pre: pre,
+				post: post,
+				color: color
+			};
+			return this;
+		}
+
+		return this._barText;
+	},
+
+	max: function (val) {
+		if (val !== undefined) {
+			this._max = val;
+			return this;
+		}
+
+		return this._max;
+	},
+
+	min: function (val) {
+		if (val !== undefined) {
+			this._min = val;
+			return this;
+		}
+
+		return this._min;
+	},
+
+	progess: function (val) {
+		if (val !== undefined) {
+			this._progress = val;
+			return this;
+		}
+
+		return this._progress;
+	},
+
+	render: function (ctx) {
+		var min = this._min,
+			max = this._max,
+			progress = this._progress,
+			interval = this.geometry.x / (max - min),
+			barWidth = (progress - min) * interval;
+
+		// Check the value is not out of range
+		if (progress > max) {
+			progress = max;
+		}
+
+		if (progress < min) {
+			progress = min;
+		}
+
+		// Draw bar fill
+		if (this._barBackColor) {
+			ctx.fillStyle = this._barBackColor;
+			ctx.fillRect(-this.geometry.x2, -this.geometry.y2, this.geometry.x, this.geometry.y);
+		}
+
+		// Draw bar
+		if (this._barColor) {
+			ctx.fillStyle = this._barColor;
+			ctx.fillRect(-this.geometry.x2, -this.geometry.y2, barWidth, this.geometry.y);
+		}
+
+		// Draw bar border
+		if (this._barBorderColor) {
+			ctx.strokeStyle = this._barBorderColor;
+			ctx.strokeRect(-this.geometry.x2, -this.geometry.y2, this.geometry.x, this.geometry.y);
+		}
+
+		// Draw bar text centered
+		if (this._barText) {
 			ctx.textAlign = 'center';
 			ctx.textBaseline = 'middle';
-			
-			// Check the value is not out of range
-			if (progress['val'] > progress['max']) {
-				progress['val'] = progress['max'];
-			}
-			
-			if (progress['val'] < progress['min']) {
-				progress['val'] = progress['min'];
-			}
-			
-			// Draw bar fill
-			if (progress['fill']) {
-				ctx.fillStyle = progress['fill'].color || '#000000';
-				ctx.fillRect(0, 0, entity._width, entity._height);
-			}
-			
-			// Draw bar
-			if (progress['bar']) {
-				var inverval = entity._width / (progress['max'] - progress['min']);
-				var barWidth = (progress['val'] - progress['min']) * inverval;
-				
-				ctx.fillStyle = progress['bar'].color || '#fff600';
-				ctx.fillRect(0, 0, barWidth, entity._height);
-			}
-			
-			// Draw bar border
-			if (progress['border']) {
-				ctx.strokeStyle = progress['border'].color || '#ffffff';
-				ctx.strokeRect(0, 0, entity._width, entity._height);
-			}
-			
-			// Draw bar text centered
-			if (progress['text']) {
-				ctx.translate(entity._width / 2, entity._height / 2);
-				ctx.fillStyle = progress['text'].color || '#ffffff';
-				ctx.fillText(progress['text'].pre + String(progress.val) + progress['text'].post, 0, 0);
-			}
+
+			ctx.fillStyle = this._barText.color;
+			ctx.fillText(this._barText.pre + String(progress.val) + this._barText.post, 0, 0);
 		}
+	},
+
+	tick: function (ctx) {
+		this._transformContext(ctx);
+		this.render(ctx);
+		this._super(ctx, true);
 	}
-}
+});
