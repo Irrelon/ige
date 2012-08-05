6f3230b5f5885b2fbfc7cf50a77b108bd4034d10
Rob Evans <rob@irrelon.com>
2012-08-05 19:52:13 +0100

    Moved variable declaration to top of tick method.

--- engine/core/IgeInteractiveEntity.js
+++ engine/core/IgeInteractiveEntity.js
@@ -6,12 +6,13 @@ var IgeInteractiveEntity = IgeEntity.extend([
 	 */
 	tick: function (ctx, dontTransform) {
 		//this._mousePos = this.mousePos();
-		var mp = ige._mousePos;
+		var mp = ige._mousePos,
+			aabb, mouseX, mouseY;
 
 		if (mp) {
-			var	aabb = this.aabb(),
-				mouseX = mp.x,
-				mouseY = mp.y;
+			aabb = this.aabb();
+			mouseX = mp.x;
+			mouseY = mp.y;
 
 			// Check if the current mouse position is inside this aabb
 			if (aabb && (aabb.x <= mouseX && aabb.y <= mouseY && aabb.x + aabb.width > mouseX && aabb.y + aabb.height > mouseY)) {
@@ -29,10 +30,6 @@ var IgeInteractiveEntity = IgeEntity.extend([
 					// The mouse went up
 					this._handleMouseUp();
 				}
-
-				/*ctx.fillStyle = '#ffffff';
-				ctx.fillText('Entity M (' + mp.x + ', ' + mp.y + ')', 0, 0);
-				ctx.fillText('Entity P (' + aabb.x + ', ' + aabb.y + ')', 0, 15);*/
 			} else {
 				if (ige.input.mouseMove) {
 					this._handleMouseOut();
