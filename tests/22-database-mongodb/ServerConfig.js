b85c2171061392efac1ccea7d162714a279b4d95
Rob Evans <rob@irrelon.com>
2012-08-06 11:51:33 +0100

    Added mongodb example test

---
+++ tests/22-database-mongodb/ServerConfig.js
@@ -0,0 +1,14 @@
+var config = {
+	include: [
+		//{name: 'ServerNetworkEvents', path: './gameClasses/ServerNetworkEvents'}
+	],
+	db: {
+		type: 'mongo',
+		host: 'localhost',
+		user: '',
+		pass: '',
+		dbName: 'test'
+	}
+};
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = config; }
