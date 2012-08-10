e280e926556cb4274b27279aa7d356d7cdc04c29
Rob Evans <rob@irrelon.com>
2012-08-10 19:24:31 +0100

    Added enabling / disabling the create menu based on the scenegraph selection so that only valid parent mounts can be created on.

--- tests/200-editor/panels/MenuBar/index.js
+++ tests/200-editor/panels/MenuBar/index.js
@@ -6,6 +6,15 @@ MenuBarPanel = IgeClass.extend({
 			success: function (data) {
 				$('#menuBar').append(data);
 				$("#menu").kendoMenu();
+				var menu = $("#menu").data("kendoMenu");
+				menu.getItem = function (itemIndex) {
+					return menu.element.children("li").eq(itemIndex);
+				};
+
+				// Disable the create menu by default
+				menu.enable(menu.getItem(2), false);
+				// TODO: Language
+				$(menu.getItem(2)).attr('title', 'Please select a parent object from the SceneGraph before attempting to create something!');
 			},
 			dataType: 'html'
 		});
