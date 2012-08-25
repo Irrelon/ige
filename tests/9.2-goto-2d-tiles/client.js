410aa27b1168ceeb8384c2f1c2d069ded031b3ab
Rob Evans <rob@irrelon.com>
2012-08-25 17:27:28 +0100

    Added UI entity to test correct click detection.

--- tests/9.2-goto-2d-tiles/client.js
+++ tests/9.2-goto-2d-tiles/client.js
@@ -13,14 +13,23 @@ var Client = IgeClass.extend({
 			// Check if the engine started successfully
 			if (success) {
 				// Create the scene
-				self.scene1 = new IgeScene2d()
-					.drawBounds(false);
+				self.mainScene = new IgeScene2d();
+				self.objectScene = new IgeScene2d()
+					.depth(0)
+					.drawBounds(false)
+					.mount(self.mainScene);
+
+				self.uiScene = new IgeScene2d()
+					.depth(1)
+					.drawBounds(false)
+					.ignoreCamera(true) // We don't want the UI scene to be affected by the viewport's camera
+					.mount(self.mainScene);
 
 				// Create the main viewport
 				self.vp1 = new IgeViewport()
 					.id('vp1')
 					.autoSize(true)
-					.scene(self.scene1)
+					.scene(self.mainScene)
 					.drawMouse(true)
 					.drawBounds(true)
 					.mount(ige);
@@ -33,7 +42,7 @@ var Client = IgeClass.extend({
 					.drawGrid(3)
 					.drawMouse(true)
 					.drawBounds(false)
-					.mount(self.scene1);
+					.mount(self.objectScene);
 
 				// Define a function that will be called when the
 				// mouse cursor moves over one of our entities
@@ -60,6 +69,26 @@ var Client = IgeClass.extend({
 					.mouseOver(overFunc)
 					.mouseOut(outFunc)
 					.mount(self.tileMap1);
+
+				// Create a UI entity so we can test if clicking the entity will stop
+				// event propagation down to moving the player. If it's working correctly
+				// the player won't move when the entity is clicked.
+				self.button1 = new IgeUiEntity()
+					.id('testUiEntity')
+					.depth(1)
+					.backgroundColor('#474747')
+					.top(0)
+					.left(0)
+					.width('100%')
+					.height(30)
+					.borderTopColor('#666666')
+					.borderTopWidth(1)
+					.backgroundPosition(0, 0)
+					.mouseOver(function () {this.backgroundColor('#49ceff'); ige.input.stopPropagation(); })
+					.mouseOut(function () {this.backgroundColor('#474747'); ige.input.stopPropagation(); })
+					.mouseMove(function () { ige.input.stopPropagation(); })
+					.mouseUp(function () { console.log('Clicked ' + this.id()); ige.input.stopPropagation(); })
+					.mount(self.uiScene);
 
 				// Set the camera to track the character with some
 				// tracking smoothing turned on (100)
