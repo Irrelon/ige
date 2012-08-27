d18a5bb6bf5ec86a5570cb4ba6925a5b38a8c6a2
Rob Evans <rob@irrelon.com>
2012-08-27 22:18:03 +0100

    Added copy of the 7.3 test to convert to using path-finding instead of keyboard support

---
+++ tests/9.4-goto-path-finder/gameClasses/ClientNetworkEvents.js
@@ -0,0 +1,5 @@
+var ClientNetworkEvents = {
+	example: function (data) {}
+};
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = ClientNetworkEvents; }
