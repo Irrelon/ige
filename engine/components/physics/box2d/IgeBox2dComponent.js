166a9acb3efee97688402d451880cc91899bd9b6
Rob Evans <rob@irrelon.com>
2012-09-05 20:00:35 +0100

    Updated to work server-side

--- engine/components/physics/box2d/IgeBox2dComponent.js
+++ engine/components/physics/box2d/IgeBox2dComponent.js
@@ -6,23 +6,21 @@ var IgeBox2dComponent = IgeEventingClass.extend({
 		this._entity = entity;
 		this._options = options;
 
-		if (!ige.isServer) {
-			this.b2Color = Box2D.Common.b2Color;
-			this.b2Vec2 = Box2D.Common.Math.b2Vec2;
-			this.b2Math = Box2D.Common.Math.b2Math;
-			this.b2Shape = Box2D.Collision.Shapes.b2Shape;
-			this.b2BodyDef = Box2D.Dynamics.b2BodyDef;
-			this.b2Body = Box2D.Dynamics.b2Body;
-			this.b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-			this.b2Fixture = Box2D.Dynamics.b2Fixture;
-			this.b2World = Box2D.Dynamics.b2World;
-			this.b2MassData = Box2D.Collision.Shapes.b2MassData;
-			this.b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-			this.b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-			this.b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
-			this.b2ContactListener = Box2D.Dynamics.b2ContactListener;
-			this.b2Distance = Box2D.Collision.b2Distance;
-		}
+		this.b2Color = Box2D.Common.b2Color;
+		this.b2Vec2 = Box2D.Common.Math.b2Vec2;
+		this.b2Math = Box2D.Common.Math.b2Math;
+		this.b2Shape = Box2D.Collision.Shapes.b2Shape;
+		this.b2BodyDef = Box2D.Dynamics.b2BodyDef;
+		this.b2Body = Box2D.Dynamics.b2Body;
+		this.b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+		this.b2Fixture = Box2D.Dynamics.b2Fixture;
+		this.b2World = Box2D.Dynamics.b2World;
+		this.b2MassData = Box2D.Collision.Shapes.b2MassData;
+		this.b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+		this.b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+		this.b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+		this.b2ContactListener = Box2D.Dynamics.b2ContactListener;
+		this.b2Distance = Box2D.Collision.b2Distance;
 
 		this._active = true;
 		this._sleep = true;
@@ -33,6 +31,8 @@ var IgeBox2dComponent = IgeEventingClass.extend({
 
 		// Add the box2d behaviour to the ige
 		ige.addBehaviour('box2dStep', this._behaviour);
+
+		this.log('Physics component initiated!');
 	},
 
 	/**
@@ -82,6 +82,8 @@ var IgeBox2dComponent = IgeEventingClass.extend({
 			this._gravity,
 			this._sleep
 		);
+
+		this.log('World created');
 
 		return this._entity;
 	},
@@ -335,3 +337,5 @@ var IgeBox2dComponent = IgeEventingClass.extend({
 		}
 	}
 });
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = IgeBox2dComponent; }
