84441c9cff43a79089289a32eb325720d6364adb
Rob Evans <rob@irrelon.com>
2012-09-09 22:30:54 +0100

    Added an entity to test streaming

--- tests/24-network-stream/server.js
+++ tests/24-network-stream/server.js
@@ -21,6 +21,28 @@ var Server = IgeClass.extend({
 					if (success) {
 						// Accept incoming network connections
 						ige.network.acceptConnections(true);
+
+						// Create the scene
+						self.scene1 = new IgeScene2d();
+
+						// Create the main viewport and set the scene
+						// it will "look" at as the new scene1 we just
+						// created above
+						self.vp1 = new IgeViewport()
+							.autoSize(true)
+							.scene(self.scene1)
+							.drawBounds(true)
+							.mount(ige);
+
+						// Create an entity and mount it to the scene
+						self.obj = new Rotator()
+							.id('fairy1')
+							.depth(1)
+							.width(100)
+							.height(100)
+							.translateTo(0, 0, 0)
+							.streamMode(1)
+							.mount(self.scene1);
 					}
 				});
 			});
