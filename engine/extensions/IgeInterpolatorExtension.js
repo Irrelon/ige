7949e42df496a0a19dceae22dbd418b795b11df0
Rob Evans <rob@irrelon.com>
2012-09-12 17:13:26 +0100

    Basic interpolator method written

--- engine/extensions/IgeInterpolatorExtension.js
+++ engine/extensions/IgeInterpolatorExtension.js
@@ -1,3 +1,20 @@
 var IgeInterpolatorExtension = {
+	/**
+	 * Calculates the current value based on the current time along
+	 * the value range.
+	 * @param startValue The value that the
+	 * @param endValue
+	 * @param startTime
+	 * @param currentTime
+	 * @param endTime
+	 * @return {Number}
+	 */
+	interpolateValue: function (startValue, endValue, startTime, currentTime, endTime) {
+		var totalValue = endValue - startValue,
+			totalTime = endTime - startTime,
+			deltaTime = totalTime - (currentTime - startTime),
+			timeRatio = deltaTime / totalTime;
 
+		return totalValue * timeRatio + startValue;
+	}
 };
