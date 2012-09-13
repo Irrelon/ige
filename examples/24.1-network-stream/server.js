6a72567e23d5c6c3a4bdf79d22f4ff1ec87a4fbe
Rob Evans <rob@irrelon.com>
2012-09-13 05:15:17 +0100

    Set the stream interval to a high value to test interpolation smoothing :)

--- examples/24.1-network-stream/server.js
+++ examples/24.1-network-stream/server.js
@@ -20,7 +20,7 @@ var Server = IgeClass.extend({
 					if (success) {
 						// Add the network stream component
 						ige.network.addComponent(IgeStreamComponent)
-							.stream.sendInterval(30) // Send a stream update once every 30 milliseconds
+							.stream.sendInterval(120) // Send a stream update once every 30 milliseconds
 							.stream.start(); // Start the stream
 
 						// Accept incoming network connections
