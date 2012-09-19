f0e843918514d423aa3d1359b6aae5a79bca706f
Rob Evans <rob@irrelon.com>
2012-09-19 22:05:30 +0100

    Sprite sheets are now working allowing arbitrary size sections of an image to be used individually for entity textures.

--- engine/core/IgeSpriteSheet.js
+++ engine/core/IgeSpriteSheet.js
@@ -7,85 +7,49 @@ var IgeSpriteSheet = IgeTexture.extend({
 	classId: 'IgeSpriteSheet',
 	IgeSpriteSheet: true,
 
-	init: function (url, horizontalCells, verticalCells) {
-		this._super(url);
-		this._cellCountX = 0;
-		this._cellCountY = 0;
+	init: function (url, cells) {
+		var self = this;
+		this.on('loaded', function () {
+			if (self.image) {
+				// Store the cell sheet image
+				self._sheetImage = this.image;
+				var i;
 
-		this.horizontalCells(horizontalCells);
-		this.verticalCells(verticalCells);
-
-		this._applyCells();
-	},
-
-	/** horizontalCells - Sets the number of horizontal cells in the sprite sheet. {
-		category:"method",
-		arguments: [{
-			name:"val",
-			type:"integer",
-			desc:"The integer count of the number of horizontal cells in the sprite sheet.",
-		}],
-	} **/
-	horizontalCells: function (val) {
-		this._cellCountX = val;
-	},
-
-	/** verticalCells - Sets the number of vertical cells in the sprite sheet. {
-		category:"method",
-		arguments: [{
-			name:"val",
-			type:"integer",
-			desc:"The integer count of the number of vertical cells in the sprite sheet.",
-		}],
-	} **/
-	verticalCells: function (val) {
-		this._cellCountY = val;
-	},
-
-	_applyCells: function () {
-		var cell,
-			imgWidth, imgHeight,
-			rows, columns,
-			cellWidth, cellHeight,
-			cellIndex,
-			xPos, yPos,
-			sectionArr,
-			sectionCount,
-			sectionIndex,
-			section,
-			sectionBounds;
-
-		// We always define a cells array and if no actual cell
-		// operation is require we just store the cords 0, 0, width, height
-		this._cells = [];
-
-		// Do we have cell data?
-		if (this._cell) {
-			// Do we have an image to use?
-			if (this.image) {
-				if (this._cell.sections) {
-					// Perform a cell by sectioning off bits of the
-					// texture with arbitrary section dimensions
-
-					/* A section array looks like this:
-						sections = [{
-							index: 1, // The cell index (base 1)
-							aabb: [0, 0, 200, 100] // x, y, width, height
-						}];
-					*/
-					sectionArr = this._cell.sections;
-					sectionCount = sectionArr.length;
-
-					for (sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++) {
-						section = sectionArr[sectionIndex];
-						this._cells[section.index] = section.aabb;
-					}
+				// Cells in the sheets always start at index
+				// 1 so move all the cells one forward
+				for (i = 0; i < cells.length; i++) {
+					self._cells[i + 1] = cells[i];
 				}
+			} else {
+				// Unable to create cells from non-image texture
+				// TODO: Low-priority - Support cell sheets from smart-textures
+				self.log('Cannot create cell-sheet because texture has not loaded an image!', 'error');
 			}
-		} else {
-			// No cell data so just store the whole image data
-			this._cells[1] = [0, 0, this._sizeX, this._sizeY];
-		}
+		});
+
+		this._super(url);
+	},
+
+	/**
+	 * Returns the total number of cells in the cell sheet.
+	 * @return {Number}
+	 */
+	cellCount: function () {
+		return this._cells.length;
+	},
+
+	/**
+	 * Returns a string containing a code fragment that when
+	 * evaluated will reproduce this object.
+	 * @return {String}
+	 */
+	stringify: function () {
+		var str = "new " + this.classId() + "('" + this.url() + "', " + this._cells.toString() + ")";
+
+		// Every object has an ID, assign that first
+		str += ".id('" + this.id() + "');";
+
+		return str;
 	},
 
 	destroy: function () {
