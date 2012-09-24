76dce2acf550004191f64388505d7a1af8582cfb
Rob Evans <rob@irrelon.com>
2012-09-24 23:33:38 +0100

    Added IgeFilter.js

---
+++ engine/core/IgeFilters.js
@@ -0,0 +1,3 @@
+// See the ige/engine/filters folder for the individual filter source
+var IgeFilters = {};
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = IgeFilters; }
