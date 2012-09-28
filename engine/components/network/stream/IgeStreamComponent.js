3a2d0561bf715cc3aeb0081177fb61b35eabc167
Rob Evans <rob@irrelon.com>
2012-09-28 21:01:23 +0100

    Removed old commented code

--- engine/components/network/stream/IgeStreamComponent.js
+++ engine/components/network/stream/IgeStreamComponent.js
@@ -208,41 +208,6 @@ var IgeStreamComponent = IgeClass.extend({
 				entity.streamSectionData(sectionArr[sectionIndex], sectionDataArr[sectionIndex]);
 			}
 		}
-		return;
-
-		while (count--) {
-			finalDataObject = {};
-			dataItem = entityMsgArray[count].split(',');
-
-			entity = this.ige.entities.byId[dataItem[0]];
-			if (entity) {
-				// Only add the update to the queue if it was sent after
-				// the latest update time
-				if (!entity._latestUpdate) { entity._latestUpdate = 0; }
-				if (typeof(entity._targetData) == 'undefined') { entity._targetData = []; }
-				if (entity._latestUpdate < time) {
-					finalDataObject._transform = [];
-					// translate
-					finalDataObject._transform[0] = parseFloat(dataItem[1]);
-					finalDataObject._transform[1] = parseFloat(dataItem[2]);
-					// scale
-					finalDataObject._transform[3] = parseFloat(dataItem[3]);
-					finalDataObject._transform[4] = parseFloat(dataItem[4]);
-					// rotate
-					finalDataObject._transform[6] = parseFloat(dataItem[5]);
-					// origin
-					finalDataObject._transform[9] = parseFloat(dataItem[6]);
-					finalDataObject._transform[10] = parseFloat(dataItem[7]);
-					// opacity
-					finalDataObject._transform[12] = parseFloat(dataItem[8]);
-
-					// We add this.ige.time.serverTimeDiff to the packet's time to convert
-					// from the server's clock time to the clients
-					entity._targetData.push([time + this.ige.time.totalServerToClientClockLatency, finalDataObject]);
-					entity._nextTarget = new Date().getTime() + this._streamInterval;
-				}
-			}
-		}
 	}
 });
 
