6c416af3e31e2abeceda5b43ca6a8f6a1ffeabef
Rob Evans <rob@irrelon.com>
2012-10-12 22:01:18 +0100

    Cleaned up bank object after testing

--- examples/15.1-isoimages/gameClasses/ClientObjects.js
+++ examples/15.1-isoimages/gameClasses/ClientObjects.js
@@ -9,8 +9,8 @@ var ClientObjects = {
 			// Setup the 3d bounds container (this)
 			this.isometric(true)
 				.mount(parent)
-				.size3d(2 * parent._tileWidth, 2 * parent._tileHeight, 0)
-				.translateToTile((tileX), (tileY), 0)
+				.size3d(2 * parent._tileWidth, 2 * parent._tileHeight, 30)
+				.translateToTile((tileX) + 0.5, (tileY) + 0.5, 0)
 				.mouseOver(function () { this.drawBounds(true); this.drawBoundsData(true); })
 				.mouseOut(function () { this.drawBounds(false); this.drawBoundsData(false); })
 				.drawBounds(false)
@@ -21,7 +21,7 @@ var ClientObjects = {
 			var texture = ige.client.gameTexture.bank,
 				halfImageHeight = texture.image.height / 2,
 				quarterImageWidth = texture.image.width / 4,
-				imageScale = 0.25,
+				imageScale = 0.3,
 				yPos = ((-halfImageHeight + quarterImageWidth) * imageScale) + (this.geometry.z2 + (this.geometry.z2 / 4));
 
 			this.imageEntity = new IgeEntity()
@@ -31,7 +31,6 @@ var ClientObjects = {
 				.drawBounds(false)
 				.drawBoundsData(false)
 				.translateTo(0, yPos, 0)
-				.drawBounds(true)
 				.mount(this);
 		}
 	}),
