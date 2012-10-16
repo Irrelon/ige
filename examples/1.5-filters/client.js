749391c6d1dad2ab5a52182e9eadb53019fb0617
Rob Evans <rob@irrelon.com>
2012-10-17 00:24:10 +0100

    Updated 1.5 example to include text headings over each filtered image to explain what it is

--- examples/1.5-filters/client.js
+++ examples/1.5-filters/client.js
@@ -9,6 +9,8 @@ var Client = IgeClass.extend({
 
 		this.obj = [];
 
+		this.verdana = new IgeFontSheet('../assets/textures/fonts/verdana_10px.png', 20);
+
 		// Load the fairy texture a few times and store it in the gameTexture array
 		gameTexture[0] = new IgeTexture('../assets/textures/sprites/fairy.png');
 		gameTexture[1] = new IgeTexture('../assets/textures/sprites/fairy.png');
@@ -51,6 +53,15 @@ var Client = IgeClass.extend({
 						.translateTo(0, -150, 0)
 						.mount(self.scene1);
 
+					new IgeFontEntity()
+						.id('title0')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Original Image')
+						.translateTo(0, -220, 0)
+						.drawBounds(false)
+						.mount(self.scene1);
+
 					// Create a second rotator entity and mount
 					// it to the first one at 0, 50 relative to the
 					// parent
@@ -63,6 +74,15 @@ var Client = IgeClass.extend({
 						.translateTo(-300, 0, 0)
 						.mount(self.scene1);
 
+					new IgeFontEntity()
+						.id('title1')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Greyscale')
+						.translateTo(-300, -70, 0)
+						.drawBounds(false)
+						.mount(self.scene1);
+
 					// Create some more fairies and mount them to the scene
 					self.obj[2] = new Rotator()
 						.id('fairy2')
@@ -71,6 +91,15 @@ var Client = IgeClass.extend({
 						.height(100)
 						.texture(gameTexture[2])
 						.translateTo(-150, 0, 0)
+						.mount(self.scene1);
+
+					new IgeFontEntity()
+						.id('title2')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Brighten')
+						.translateTo(-150, -70, 0)
+						.drawBounds(false)
 						.mount(self.scene1);
 
 					self.obj[3] = new Rotator()
@@ -82,6 +111,15 @@ var Client = IgeClass.extend({
 						.translateTo(0, 0, 0)
 						.mount(self.scene1);
 
+					new IgeFontEntity()
+						.id('title3')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Threshold')
+						.translateTo(0, -70, 0)
+						.drawBounds(false)
+						.mount(self.scene1);
+
 					self.obj[4] = new Rotator()
 						.id('fairy4')
 						.depth(1)
@@ -89,6 +127,15 @@ var Client = IgeClass.extend({
 						.height(100)
 						.texture(gameTexture[4])
 						.translateTo(150, 0, 0)
+						.mount(self.scene1);
+
+					new IgeFontEntity()
+						.id('title4')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Sharpen')
+						.translateTo(150, -70, 0)
+						.drawBounds(false)
 						.mount(self.scene1);
 
 					self.obj[5] = new Rotator()
@@ -100,6 +147,15 @@ var Client = IgeClass.extend({
 						.translateTo(300, 0, 0)
 						.mount(self.scene1);
 
+					new IgeFontEntity()
+						.id('title5')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Blur')
+						.translateTo(300, -70, 0)
+						.drawBounds(false)
+						.mount(self.scene1);
+
 					self.obj[6] = new Rotator()
 						.id('fairy6')
 						.depth(1)
@@ -107,6 +163,15 @@ var Client = IgeClass.extend({
 						.height(100)
 						.texture(gameTexture[6])
 						.translateTo(-300, 150, 0)
+						.mount(self.scene1);
+
+					new IgeFontEntity()
+						.id('title6')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Emboss')
+						.translateTo(-300, 80, 0)
+						.drawBounds(false)
 						.mount(self.scene1);
 
 					self.obj[7] = new Rotator()
@@ -118,6 +183,15 @@ var Client = IgeClass.extend({
 						.translateTo(-150, 150, 0)
 						.mount(self.scene1);
 
+					new IgeFontEntity()
+						.id('title7')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Edge Detect')
+						.translateTo(-150, 80, 0)
+						.drawBounds(false)
+						.mount(self.scene1);
+
 					self.obj[8] = new Rotator()
 						.id('fairy8')
 						.depth(1)
@@ -125,6 +199,15 @@ var Client = IgeClass.extend({
 						.height(100)
 						.texture(gameTexture[8])
 						.translateTo(0, 150, 0)
+						.mount(self.scene1);
+
+					new IgeFontEntity()
+						.id('title8')
+						.texture(self.verdana)
+						.textAlignX(1)
+						.text('Color Overlay')
+						.translateTo(0, 80, 0)
+						.drawBounds(false)
 						.mount(self.scene1);
 
 					// Apply a greyscale filter to JUST the first fairy texture
