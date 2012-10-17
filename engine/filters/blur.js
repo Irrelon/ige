080f9622b2156d0d8d2db38cfaa3886a3f778cb8
Rob Evans <rob@irrelon.com>
2012-10-17 03:11:10 +0100

    Added blur strength support

--- engine/filters/blur.js
+++ engine/filters/blur.js
@@ -1,21 +1,30 @@
-IgeFilters.blur = function (canvas, ctx, originalImage, texture) {
+IgeFilters.blur = function (canvas, ctx, originalImage, texture, data) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.drawImage(originalImage, 0, 0);
 
-	// Apply the filter and then put the new pixel data
-	ctx.putImageData(
-		IgeFilters._convolute(
-			ctx.getImageData(
-				0,
-				0,
-				canvas.width,
-				canvas.height
+	var strength = 1,
+		loop;
+
+	if (data && data.value) {
+		strength = data.value;
+	}
+
+	for (loop = 0; loop < strength; loop++) {
+		// Apply the filter and then put the new pixel data
+		ctx.putImageData(
+			IgeFilters._convolute(
+				ctx.getImageData(
+					0,
+					0,
+					canvas.width,
+					canvas.height
+				),
+				[ 1/9, 1/9, 1/9,
+					1/9, 1/9, 1/9,
+					1/9, 1/9, 1/9 ]
 			),
-			[ 1/9, 1/9, 1/9,
-				1/9, 1/9, 1/9,
-				1/9, 1/9, 1/9 ]
-		),
-		0,
-		0
-	);
+			0,
+			0
+		);
+	}
 };
