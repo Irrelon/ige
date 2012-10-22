169221f717b6109226fb5590290f521979f41525
Rob Evans <rob@irrelon.com>
2012-10-22 20:24:19 +0100

    Returned path-finding example to original settings (mostly)

--- examples/9.5-goto-path-finder/client.js
+++ examples/9.5-goto-path-finder/client.js
@@ -171,16 +171,16 @@ var Client = IgeClass.extend({
 				// Generate first path, diagonal enabled
 				var path1, path2, path3, path4;
 
-				path1 = self.pathFinder.aStar(self.tileMap1, new IgePoint(0, 0, 0), new IgePoint(3, 0, 0), function (tileData, tileX, tileY) {
+				path1 = self.pathFinder.aStar(self.tileMap1, new IgePoint(0, 0, 0), new IgePoint(2, 0, 0), function (tileData, tileX, tileY) {
 					// If the map tile data is set to 1, don't allow a path along it
 					return tileData !== 1;
 				}, true, true);
 
 				// Generate first path, diagonal disabled
-				path2 = self.pathFinder.aStar(self.tileMap1, new IgePoint(3, 0, 0), new IgePoint(6, 4, 0), function (tileData, tileX, tileY) {
+				path2 = self.pathFinder.aStar(self.tileMap1, new IgePoint(2, 0, 0), new IgePoint(6, 4, 0), function (tileData, tileX, tileY) {
 					// If the map tile data is set to 1, don't allow a path along it
 					return tileData !== 1;
-				}, true, false);
+				}, true, true);
 
 				// Generate first path, diagonal enabled
 				path3 = self.pathFinder.aStar(self.tileMap1, new IgePoint(6, 4, 0), new IgePoint(7, 0, 0), function (tileData, tileX, tileY) {
@@ -192,17 +192,17 @@ var Client = IgeClass.extend({
 				path4 = self.pathFinder.aStar(self.tileMap1, new IgePoint(7, 0, 0), new IgePoint(0, 0, 0), function (tileData, tileX, tileY) {
 					// If the map tile data is set to 1, don't allow a path along it
 					return tileData !== 1;
-				}, true, false);
+				}, true, true);
 
 				// Assign the path to the player
 				self.player
 					.path.drawPath(true) // Enable debug drawing the paths
 					.path.drawPathGlow(true) // Enable path glowing (eye candy)
-					.path.drawPathText(true); // Enable path text output
-					//.path.add(path1)
-					//.path.add(path2)
-					//.path.add(path3)
-					//.path.add(path4);
+					.path.drawPathText(true) // Enable path text output
+					.path.add(path1)
+					.path.add(path2)
+					.path.add(path3)
+					.path.add(path4);
 
 				// Register some event listeners for the path
 				self.player.path.on('started', function () { console.log('Pathing started...'); });
