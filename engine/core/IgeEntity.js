78b3a2e8d74729a043a44af708ae84f28cf8157b
Rob Evans <rob@irrelon.com>
2012-10-24 16:20:53 +0100

    Added stream and interpolator code directly into IgeEntity.js, altered destroy() to make sure the destruction of the entity is sent across the stream.

--- engine/core/IgeEntity.js
+++ engine/core/IgeEntity.js
@@ -3,9 +3,7 @@
  */
 var IgeEntity = IgeObject.extend([
 	{extension: IgeTransformExtension, overwrite: false},
-	{extension: IgeUiInteractionExtension, overwrite: true},
-	{extension: IgeStreamExtension, overwrite: false},
-	{extension: IgeInterpolatorExtension, overwrite: true}
+	{extension: IgeUiInteractionExtension, overwrite: true}
 ], {
 	classId: 'IgeEntity',
 
@@ -245,12 +243,12 @@ var IgeEntity = IgeObject.extend([
 	 * Gets / sets the life span of the object in milliseconds. The life
 	 * span is how long the object will exist for before being automatically
 	 * destroyed.
-	 * @param {Number=} val
+	 * @param {Number=} milliseconds
 	 * @return {*} Returns this when setting the value or the current value if none is specified.
 	 */
-	lifeSpan: function (val) {
-		if (val !== undefined) {
-			this.deathTime(new Date().getTime() + val);
+	lifeSpan: function (milliseconds) {
+		if (milliseconds !== undefined) {
+			this.deathTime(new Date().getTime() + milliseconds);
 			return this;
 		}
 
@@ -873,6 +871,429 @@ var IgeEntity = IgeObject.extend([
 		}
 
 		return str;
+	},
+
+	destroy: function () {
+		this._alive = false;
+		this.emit('destroyed', this);
+
+		/* CEXCLUDE */
+		// Check if the entity is streaming
+		if (this._streamMode === 1) {
+			delete this._streamDataCache;
+			this.streamSync();
+		}
+		/* CEXCLUDE */
+
+		// Call IgeObject.destroy()
+		this._super();
+	},
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// STREAM
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/* CEXCLUDE */
+	/**
+	 * Gets / sets the stream mode that the stream system will use when
+	 * handling pushing data updates to connected clients.
+	 * @param {Number=} val A value representing the stream mode.
+	 * @return {*}
+	 */
+	streamMode: function (val) {
+		if (val !== undefined) {
+			this._streamMode = val;
+			return this;
+		}
+
+		return this._streamMode;
+	},
+
+	/**
+	 * Gets / sets the stream control callback function that will be called
+	 * each time the entity tick method is called and stream-able data is
+	 * updated.
+	 * @param {Function=} method
+	 * @return {*}
+	 */
+	streamControl: function (method) {
+		if (method !== undefined) {
+			this._streamControl = method;
+			return this;
+		}
+
+		return this._streamControl;
+	},
+
+	/**
+	 * Gets / sets the stream control call interval. This value
+	 * is in milliseconds and cannot be lower than 16.
+	 * @param {Number=} val Number of milliseconds between calls
+	 * to the streamControl method.
+	 * @return {*}
+	 */
+	streamControlInterval: function (val) {
+		if (method !== undefined) {
+			if (val < 16) {
+				delete this._streamControlInterval;
+			} else {
+				this._streamControlDelta = 0;
+				this._streamControlInterval = val;
+			}
+			return this;
+		}
+
+		return this._streamControlInterval;
+	},
+
+	/**
+	 * Gets / sets the precision by which floating-point values will
+	 * be encoded and sent when packaged into stream data.
+	 * @param val
+	 * @return {*}
+	 */
+	streamFloatPrecision: function (val) {
+		if (val !== undefined) {
+			this._streamFloatPrecision = val;
+
+			var i, floatRemove = '\\.';
+
+			// Update the floatRemove regular expression pattern
+			for (i = 0; i < this._streamFloatPrecision; i++) {
+				floatRemove += '0';
+			}
+
+			// Add the trailing comma
+			floatRemove += ',';
+
+			// Create the new regexp
+			this._floatRemoveRegExp = new RegExp(floatRemove, 'g');
+
+			return this;
+		}
+
+		return this._streamFloatPrecision;
+	},
+
+	/**
+	 * Queues stream data for this entity to be sent to the specified client id or array
+	 * of client ids.
+	 * @param {String, Array} clientId Either a string ID or an array of string IDs of
+	 * each client to send the stream data to.
+	 * @return {*}
+	 */
+	streamSync: function (clientId) {
+		if (this._streamMode === 1) {
+			// Stream mode is automatic so check for the
+			// control method
+			if (this._streamControl) {
+				// Check if we have a stream control interval
+				if (this._streamControlInterval) {
+					this._streamControlDelta += ige.tickDelta;
+
+					if (this._streamControlDelta < this._streamControlInterval) {
+						// The stream control interval is still higher than
+						// the stream control delta so exit without calling the
+						// stream control method
+						return this;
+					} else {
+						// We've reached the delta we want so zero it now
+						// ready for the next loop
+						this._streamControlDelta = 0;
+					}
+				}
+				// Stream control method exists, loop clients and call
+				// the control method to see if data should be streamed
+
+				// Grab an array of connected clients from the network
+				// system
+				var clientArr = ige.network.clients(),
+					i;
+
+				for (i in clientArr) {
+					if (clientArr.hasOwnProperty(i)) {
+						// Call the callback method and if it returns true,
+						// send the stream data to this client
+						if (this._streamControl.apply(this, [i])) {
+							this._streamSync(i);
+						}
+					}
+				}
+
+				return this;
+			} else {
+				// Stream control method does not exist, send data
+				// to all connected clients now
+				this._streamSync();
+				return this;
+			}
+		}
+
+		if (this._streamMode === 2) {
+			// Stream mode is advanced
+			this._streamSync(clientId);
+
+			return this;
+		}
+
+		return this;
+	},
+
+	/**
+	 * Gets / sets the array of sections that this entity will
+	 * encode into its stream data.
+	 * @param {Array=} sectionArray An array of strings.
+	 * @return {*}
+	 */
+	streamSections: function (sectionArray) {
+		if (sectionArray !== undefined) {
+			this._streamSections = sectionArray;
+			return this;
+		}
+
+		return this._streamSections;
+	},
+
+	/**
+	 * Gets / sets the data for the specified data section id.
+	 * @param {String} sectionId A string identifying the section to handle data get / set for.
+	 * @param {*=} data If present, this is the data that has been sent from the server to the client for this entity.
+	 * @return {*}
+	 */
+	streamSectionData: function (sectionId, data) {
+		if (sectionId === 'transform') {
+			if (data) {
+				// We have received updated data
+				// Translate
+				var dataArr = data.split(',');
+				if (dataArr[0]) { dataArr[0] = parseFloat(dataArr[0]); }
+				if (dataArr[1]) { dataArr[1] = parseFloat(dataArr[1]); }
+				if (dataArr[2]) { dataArr[2] = parseFloat(dataArr[2]); }
+
+				// Scale
+				if (dataArr[3]) { dataArr[3] = parseFloat(dataArr[3]); }
+				if (dataArr[4]) { dataArr[4] = parseFloat(dataArr[4]); }
+				if (dataArr[5]) { dataArr[5] = parseFloat(dataArr[5]); }
+
+				// Rotate
+				if (dataArr[6]) { dataArr[6] = parseFloat(dataArr[6]); }
+				if (dataArr[7]) { dataArr[7] = parseFloat(dataArr[7]); }
+				if (dataArr[8]) { dataArr[8] = parseFloat(dataArr[8]); }
+
+				// Add it to the time stream
+				this._timeStream.push([ige.network.stream._streamDataTime, dataArr]);
+
+				// Check stream length, don't allow higher than 20 items
+				if (this._timeStream.length > 20) {
+					// Remove the first item
+					this._timeStream.shift();
+				}
+			} else {
+				// We should return stringified data
+				return this._translate.toString(this._streamFloatPrecision) + ',' + // translate
+					this._scale.toString(this._streamFloatPrecision) + ',' + // scale
+					this._rotate.toString(this._streamFloatPrecision) + ','; // rotate
+			}
+		}
+	},
+
+	/**
+	 * Asks the stream system to queue the stream data to
+	 * the specified client id or array of ids.
+	 * @param clientId
+	 * @private
+	 */
+	_streamSync: function (clientId) {
+		ige.network.stream.queue(this.id(), this._streamData(), clientId);
+	},
+
+	/**
+	 * Generates and returns the current stream data for this entity. The
+	 * data will usually include only properties that have changed since
+	 * the last time the stream data was generated. The returned data is
+	 * a string that has been compressed in various ways to reduce network
+	 * overhead during transmission.
+	 * @return {String}
+	 * @private
+	 */
+	_streamData: function () {
+		// Check if we already have a cached version of the streamData
+		if (this._streamDataCache) {
+			return this._streamDataCache;
+		} else {
+			// Let's generate our stream data
+			var streamData = '',
+				sectionDataString = '',
+				sectionArr = this._streamSections,
+				sectionCount = sectionArr.length,
+				sectionData,
+				sectionIndex,
+				aliveInt = this._alive ? 1: 0;
+
+			// Add the default data (id and class)
+			streamData += this.id() + ',' + this._parent.id() + ',' + this.classId() + ',' + aliveInt;
+
+			// Only send further data if the entity is still "alive"
+			if (this._alive) {
+				// Now loop the data sections array and compile the rest of the
+				// data string from the data section return data
+				for (sectionIndex = 0; sectionIndex < sectionCount; sectionIndex++) {
+					// Get the section data for this section id
+					sectionData = this.streamSectionData(sectionArr[sectionIndex]);
+
+					// Add the section start designator character. We do this
+					// regardless of if there is actually any section data because
+					// we want to be able to identify sections in a serial fashion
+					// on receipt of the data string on the client
+					sectionDataString += '|';
+
+					// Check if we were returned any data
+					if (sectionData) {
+						// Add the data to the section string
+						sectionDataString += sectionData;
+					}
+				}
+
+				// Add any custom data to the stream string at this point
+				if (sectionDataString) {
+					streamData += sectionDataString;
+				}
+
+				// Remove any .00 from the string since we don't need that data
+				// TODO: What about if a property is a string with something.00 and it should be kept?
+				streamData = streamData.replace(this._floatRemoveRegExp, ',');
+			}
+
+			// Store the data in cache in case we are asked for it again this tick
+			// the tick() method of the IgeEntity class clears this every tick
+			this._streamDataCache = streamData;
+
+			return streamData;
+		}
+	},
+	/* CEXCLUDE */
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// INTERPOLATOR
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Calculates the current value based on the time along the
+	 * value range.
+	 * @param startValue The value that the interpolation started from.
+	 * @param endValue The target value to be interpolated to.
+	 * @param startTime The time the interpolation started.
+	 * @param currentTime The current time.
+	 * @param endTime The time the interpolation will end.
+	 * @return {Number}
+	 */
+	interpolateValue: function (startValue, endValue, startTime, currentTime, endTime) {
+		var totalValue = endValue - startValue,
+			totalTime = endTime - startTime,
+			deltaTime = totalTime - (currentTime - startTime),
+			timeRatio = deltaTime / totalTime;
+
+		return endValue - (totalValue * timeRatio);
+	},
+
+	_processInterpolate: function (renderTime, maxLerp) {
+		// Set the maximum lerp to 200 if none is present
+		if (!maxLerp) { maxLerp = 200; }
+
+		var maxLerpSquared = maxLerp * maxLerp,
+			previousData,
+			nextData,
+			timeStream = this._timeStream,
+			dataDelta,
+			offsetDelta,
+			currentTime,
+			previousTransform,
+			nextTransform,
+			currentTransform = [],
+			i = 1;
+
+		// Find the point in the time stream that is
+		// closest to the render time and assign the
+		// previous and next data points
+		while (timeStream[i]) {
+			if (timeStream[i][0] >= renderTime) {
+				// We have previous and next data points from the
+				// time stream so store them
+				previousData = timeStream[i - 1];
+				nextData = timeStream[i];
+				break;
+			}
+			i++;
+		}
+
+		// Check if we have some data to use
+		if (!nextData && !previousData) {
+			// No in-time data was found, check for lagging data
+			if (timeStream.length > 2) {
+				if (timeStream[timeStream.length - 1][0] < renderTime) {
+					// Lagging data is available, use that
+					previousData = timeStream[timeStream.length - 2];
+					nextData = timeStream[timeStream.length - 1];
+					timeStream.shift();
+					this.emit('entityInterpolationLag');
+				}
+			}
+		} else {
+			// We have some new data so clear the old data
+			timeStream.splice(0, i - 1);
+		}
+
+		// If we have data to use
+		if (nextData && previousData) {
+			// Calculate the delta times
+			dataDelta = nextData[0] - previousData[0];
+			offsetDelta = renderTime - previousData[0];
+
+			// Calculate the current time between the two data points
+			currentTime = offsetDelta / dataDelta;
+
+			// Clamp the current time from 0 to 1
+			if (currentTime < 0) { currentTime = 0.0; } else if (currentTime > 1) { currentTime = 1.0; }
+
+			// Set variables up to store the previous and next data
+			previousTransform = previousData[1];
+			nextTransform = nextData[1];
+
+			// Translate
+			currentTransform[0] = this.interpolateValue(previousTransform[0], nextTransform[0], previousData[0], renderTime, nextData[0]);
+			currentTransform[1] = this.interpolateValue(previousTransform[1], nextTransform[1], previousData[0], renderTime, nextData[0]);
+			currentTransform[2] = this.interpolateValue(previousTransform[2], nextTransform[2], previousData[0], renderTime, nextData[0]);
+			// Scale
+			currentTransform[3] = this.interpolateValue(previousTransform[3], nextTransform[3], previousData[0], renderTime, nextData[0]);
+			currentTransform[4] = this.interpolateValue(previousTransform[4], nextTransform[4], previousData[0], renderTime, nextData[0]);
+			currentTransform[5] = this.interpolateValue(previousTransform[5], nextTransform[5], previousData[0], renderTime, nextData[0]);
+			// Rotate
+			currentTransform[6] = this.interpolateValue(previousTransform[6], nextTransform[6], previousData[0], renderTime, nextData[0]);
+			currentTransform[7] = this.interpolateValue(previousTransform[7], nextTransform[7], previousData[0], renderTime, nextData[0]);
+			currentTransform[8] = this.interpolateValue(previousTransform[8], nextTransform[8], previousData[0], renderTime, nextData[0]);
+
+			this.translateTo(parseFloat(currentTransform[0]), parseFloat(currentTransform[1]), parseFloat(currentTransform[2]));
+			this.scaleTo(parseFloat(currentTransform[3]), parseFloat(currentTransform[4]), parseFloat(currentTransform[5]));
+			this.rotateTo(parseFloat(currentTransform[6]), parseFloat(currentTransform[7]), parseFloat(currentTransform[8]));
+
+			/*// Calculate the squared distance between the previous point and next point
+			 dist = this.distanceSquared(previousTransform.x, previousTransform.y, nextTransform.x, nextTransform.y);
+
+			 // Check that the distance is not higher than the maximum lerp and if higher,
+			 // set the current time to 1 to snap to the next position immediately
+			 if (dist > maxLerpSquared) { currentTime = 1; }
+
+			 // Interpolate the entity position by multiplying the Delta times T, and adding the previous position
+			 currentPosition = {};
+			 currentPosition.x = ( (nextTransform.x - previousTransform.x) * currentTime ) + previousTransform.x;
+			 currentPosition.y = ( (nextTransform.y - previousTransform.y) * currentTime ) + previousTransform.y;
+
+			 // Now actually transform the entity
+			 this.translate(entity, currentPosition.x, currentPosition.y);*/
+
+			// Record the last time we updated the entity so we can disregard any updates
+			// that arrive and are before this timestamp (not applicable in TCP but will
+			// apply if we ever get UDP in websockets)
+			this._lastUpdate = new Date().getTime();
+		}
 	}
 });
 
