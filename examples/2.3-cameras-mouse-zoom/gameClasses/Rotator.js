90caa8178484537ccc4e6b72ba03e1aee0450c1d
Rob Evans <rob@irrelon.com>
2012-10-25 09:09:34 +0100

    Added mouse zoom example

---
+++ examples/2.3-cameras-mouse-zoom/gameClasses/Rotator.js
@@ -0,0 +1,10 @@
+var Rotator = IgeEntity.extend({
+	classId:'Rotator',
+
+	tick: function (ctx) {
+		this.rotateBy(0, 0, (0.1 * ige.tickDelta) * Math.PI / 180);
+		this._super(ctx);
+	}
+});
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Rotator; }
