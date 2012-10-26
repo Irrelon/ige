f43c387e0de17f5d76cbc2cf4c609f3a9ec0e441
Rob Evans <rob@irrelon.com>
2012-10-26 16:53:45 +0100

    Removed some debug console logging

--- examples/24.2-multiplayer/gameClasses/ServerNetworkEvents.js
+++ examples/24.2-multiplayer/gameClasses/ServerNetworkEvents.js
@@ -23,13 +23,11 @@ var ServerNetworkEvents = {
 
 	_onPlayerEntity: function (data, clientId) {
 		if (!ige.server.players[clientId]) {
-			console.log('Created player with id ' + clientId);
 			ige.server.players[clientId] = new Player(clientId)
 				.streamMode(1)
 				.mount(ige.server.scene1);
 
 			// Tell the client to track their player entity
-			console.log('Sending data...');
 			ige.network.send('playerEntity', ige.server.players[clientId].id(), clientId);
 		}
 	},
