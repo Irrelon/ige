13a69c4ca122dffbdf11570619fa2ddf7ca8a9b5
Rob Evans <rob@irrelon.com>
2012-10-28 12:17:14 +0000

    Fixed response arguments bug

--- examples/17.1-network-netio/client.js
+++ examples/17.1-network-netio/client.js
@@ -36,7 +36,7 @@ var Client = IgeClass.extend({
 					ige.network.send('test', {moo: 'Some test data!'});
 
 					// Send the server a request (gets a callback when the server responds!)
-					ige.network.request('testRequest', {hello:100}, function (data) {
+					ige.network.request('testRequest', {hello:100}, function (commandName, data) {
 						console.log('Request response received from server via callback with data:', data);
 					});
 				});
