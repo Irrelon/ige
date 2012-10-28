4ef86fd4b5c7d4e8d37fa41fdc8e0b6ccf617c04
Rob Evans <rob@irrelon.com>
2012-10-28 09:35:42 +0000

    Address change

--- examples/24.2-multiplayer/client.js
+++ examples/24.2-multiplayer/client.js
@@ -31,7 +31,7 @@ var Client = IgeClass.extend({
 					// than before the scene etc are created... maybe you want
 					// a splash screen or a menu first? Then connect after you've
 					// got a username or something?
-					ige.network.start('http://192.168.0.199:2000', function () {
+					ige.network.start('http://localhost:2000', function () {
 						// Setup the network command listeners
 						ige.network.define('playerEntity', self._onPlayerEntity); // Defined in ./gameClasses/ClientNetworkEvents.js
 
@@ -41,7 +41,7 @@ var Client = IgeClass.extend({
 							// Create a listener that will fire whenever an entity
 							// is created because of the incoming stream data
 							.stream.on('entityCreated', function (entity) {
-								console.log('Stream entity created with ID: ' + entity.id());
+								self.log('Stream entity created with ID: ' + entity.id());
 							});
 
 						// Create the scene
