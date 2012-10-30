72ad6532b99d36a5eb7de102591a2cc1af27ef96
Rob Evans <rob@irrelon.com>
2012-10-30 16:27:17 +0000

    Removed old textures not needed

--- examples/16.2-webgl-turret-point/client.js
+++ examples/16.2-webgl-turret-point/client.js
@@ -15,79 +15,70 @@ var Client = IgeClass.extend({
 
 		ige.input.debug(true);
 
-		// Load the fairy texture and store it in the gameTexture array
-		gameTexture[0] = new IgeTexture('../assets/textures/sprites/fairy.png');
+		// Create the HTML canvas
+		ige.createFrontBuffer(true);
 
-		// Load a smart texture
-		gameTexture[1] = new IgeTexture('../assets/textures/smartTextures/simpleBox.js');
+		// Setup the input system to use events
+		// from the three.js renderer canvas
+		ige.input.setupListeners(ige.three._canvas);
 
-		// Wait for our textures to load before continuing
-		ige.on('texturesLoaded', function () {
-			// Create the HTML canvas
-			ige.createFrontBuffer(true);
+		// Start the engine
+		ige.start(function (success) {
+			// Check if the engine started successfully
+			if (success) {
+				// Create the scene
+				self.scene1 = new IgeScene2d()
+					.id('scene1');
 
-			// Setup the input system to use events
-			// from the three.js renderer canvas
-			ige.input.setupListeners(ige.three._canvas);
+				// Create the main viewport and set the scene
+				// it will "look" at as the new scene1 we just
+				// created above
+				self.vp1 = new IgeViewport()
+					.id('vp1')
+					.autoSize(true)
+					.scene(self.scene1)
+					.drawBounds(true)
+					.mount(ige);
 
-			// Start the engine
-			ige.start(function (success) {
-				// Check if the engine started successfully
-				if (success) {
-					// Create the scene
-					self.scene1 = new IgeScene2d()
-						.id('scene1');
+				self.vp1.camera.translateTo(0, 0, 500);
 
-					// Create the main viewport and set the scene
-					// it will "look" at as the new scene1 we just
-					// created above
-					self.vp1 = new IgeViewport()
-						.id('vp1')
-						.autoSize(true)
-						.scene(self.scene1)
-						.drawBounds(true)
-						.mount(ige);
+				ige.input.mapAction('rotateLeft', ige.input.key.left);
+				ige.input.mapAction('rotateRight', ige.input.key.right);
+				ige.input.mapAction('rotateLeft', ige.input.key.a);
+				ige.input.mapAction('rotateRight', ige.input.key.d);
 
-					self.vp1.camera.translateTo(0, 0, 500);
+				self.plane = new IgeEntity()
+					.id('plane')
+					.translateTo(0, 0, 0)
+					.rotateTo(0, 0, 0)
+					.scaleTo(1, 1, 1)
+					.mesh(new THREE.PlaneGeometry(window.innerWidth * 2, window.innerHeight * 2))
+					.mount(self.scene1);
 
-					ige.input.mapAction('rotateLeft', ige.input.key.left);
-					ige.input.mapAction('rotateRight', ige.input.key.right);
-					ige.input.mapAction('rotateLeft', ige.input.key.a);
-					ige.input.mapAction('rotateRight', ige.input.key.d);
+				// Create a ship entity and mount it to the scene
+				self.obj[0] = new IgeEntity()
+					.id('ship0')
+					.translateTo(0, 250, 0)
+					.rotateTo(0, Math.radians(0), 0)
+					.scaleTo(4, 4, 4)
+					.material(new THREE.MeshFaceMaterial())
+					.model(modelSpaceFrigate6)
+					.addBehaviour('keyboardControl', ShipControl)
+					.mount(self.scene1);
 
-					self.plane = new IgeEntity()
-						.id('plane')
-						.translateTo(0, 0, 0)
-						.rotateTo(0, 0, 0)
-						.scaleTo(1, 1, 1)
-						.mesh(new THREE.PlaneGeometry(window.innerWidth * 2, window.innerHeight * 2))
-						.mount(self.scene1);
+				// Mount a turret to the ship entity
+				self.obj[1] = new IgeEntity()
+					.id('turret1')
+					.translateTo(0, -2.6, 1.8)
+					.rotateTo(0, 0, Math.radians(0))
+					.scaleTo(0.2, 0.2, 0.2)
+					.material(new THREE.MeshFaceMaterial())
+					.model(modelTurret)
+					.addBehaviour('mouseAim', TurretMouseAim)
+					.mount(self.obj[0]);
 
-					// Create a ship entity and mount it to the scene
-					self.obj[0] = new IgeEntity()
-						.id('ship0')
-						.translateTo(0, 250, 0)
-						.rotateTo(0, Math.radians(0), 0)
-						.scaleTo(4, 4, 4)
-						.material(new THREE.MeshFaceMaterial())
-						.model(modelSpaceFrigate6)
-						.addBehaviour('keyboardControl', ShipControl)
-						.mount(self.scene1);
-
-					// Mount a turret to the ship entity
-					self.obj[1] = new IgeEntity()
-						.id('turret1')
-						.translateTo(0, -2.6, 1.8)
-						.rotateTo(0, 0, Math.radians(0))
-						.scaleTo(0.2, 0.2, 0.2)
-						.material(new THREE.MeshFaceMaterial())
-						.model(modelTurret)
-						.addBehaviour('mouseAim', TurretMouseAim)
-						.mount(self.obj[0]);
-
-					self.vp1.camera.trackTranslate(self.obj[0], 10);
-				}
-			});
+				self.vp1.camera.trackTranslate(self.obj[0], 10);
+			}
 		});
 	}
 });
