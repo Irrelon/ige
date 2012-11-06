849f76f79a3517f60de49a7b2ae61ffaf554b6d6
Rob Evans <rob@irrelon.com>
2012-11-06 10:46:59 +0000

    Added blank_game to the project to provide a quick copy/paste project to edit

---
+++ blank_game/client.js
@@ -0,0 +1,83 @@
+var Client = IgeClass.extend({
+	classId: 'Client',
+	init: function () {
+		ige.showStats(1);
+
+		// Load our textures
+		var self = this,
+			gameTexture = [];
+
+		this.obj = [];
+
+		ige.input.debug(true);
+
+		// Load the fairy texture and store it in the gameTexture array
+		gameTexture[0] = new IgeTexture('../examples/assets/textures/sprites/fairy.png');
+
+		// Load a smart texture
+		gameTexture[1] = new IgeTexture('../examples/assets/textures/smartTextures/simpleBox.js');
+
+		// Wait for our textures to load before continuing
+		ige.on('texturesLoaded', function () {
+			// Create the HTML canvas
+			ige.createFrontBuffer(true);
+
+			// Start the engine
+			ige.start(function (success) {
+				// Check if the engine started successfully
+				if (success) {
+					// Create the scene
+					self.scene1 = new IgeScene2d()
+						.id('scene1');
+
+					// Create the main viewport and set the scene
+					// it will "look" at as the new scene1 we just
+					// created above
+					self.vp1 = new IgeViewport()
+						.id('vp1')
+						.autoSize(true)
+						.scene(self.scene1)
+						.drawBounds(true)
+						.mount(ige);
+
+					// Create an entity and mount it to the scene
+					// (the class Rotator is declared in ./gameClasses/Rotator.js)
+					self.obj[0] = new Rotator()
+						.id('fairy1')
+						.depth(1)
+						.width(100)
+						.height(100)
+						.texture(gameTexture[0])
+						.translateTo(0, 0, 0)
+						.mount(self.scene1);
+
+					// Create a second rotator entity and mount
+					// it to the first one at 0, 50 relative to the
+					// parent
+					self.obj[1] = new Rotator()
+						.id('fairy2')
+						.depth(1)
+						.width(50)
+						.height(50)
+						.texture(gameTexture[0])
+						.translateTo(0, 50, 0)
+						.mount(self.obj[0]);
+
+					// Create a third rotator entity and mount
+					// it to the first on at 0, -50 relative to the
+					// parent, but assign it a smart texture!
+					self.obj[2] = new Rotator()
+						.id('simpleBox')
+						.depth(1)
+						.width(50)
+						.height(50)
+						.texture(gameTexture[1])
+						.translateTo(0, -50, 0)
+						.mount(self.obj[0]);
+				}
+			});
+		});
+	}
+});
+
+if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Client; }
