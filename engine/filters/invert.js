e97cafe9ea70176f9d640859b0b66336ebe855e5
Rob Evans <rob@irrelon.com>
2012-11-13 12:03:41 +0000

    Added new filters

---
+++ engine/filters/invert.js
@@ -0,0 +1,18 @@
+IgeFilters.invert = function (canvas, ctx, originalImage, texture, data) {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(originalImage, 0, 0);
+
+	var w, h, canvasData, i;
+	w = canvas.width;
+	h = canvas.height;
+
+	canvasData = ctx.getImageData(0, 0, w, h);
+
+	for (i = 0; i < w * h * 4; i += 4)  {
+		canvasData.data[i] = 255 - canvasData.data[i];
+		canvasData.data[i+1] = 255 - canvasData.data[i+1];
+		canvasData.data[i+2] = 255 - canvasData.data[i+2];
+	}
+
+	ctx.putImageData(canvasData, 0, 0);
+};
