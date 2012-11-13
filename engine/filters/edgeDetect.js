941db01398306283039a29ec593a23b9880b5eea
Rob Evans <rob@irrelon.com>
2012-11-13 12:03:08 +0000

    Altered matrix, added threshold value option in the data.value parameter.

--- engine/filters/edgeDetect.js
+++ engine/filters/edgeDetect.js
@@ -1,10 +1,8 @@
-IgeFilters.edgeDetect = function (canvas, ctx, originalImage, texture) {
+IgeFilters.edgeDetect = function (canvas, ctx, originalImage, texture, data) {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.drawImage(originalImage, 0, 0);
 
-	// Apply the filter and then put the new pixel data
-	ctx.putImageData(
-		IgeFilters._convolute(
+	var newData = IgeFilters._convolute(
 			ctx.getImageData(
 				0,
 				0,
@@ -12,13 +10,34 @@ IgeFilters.edgeDetect = function (canvas, ctx, originalImage, texture) {
 				canvas.height
 			),
 			[
-				0, 0, -1, 0, 0,
-				0, -1, -2, -1, 0,
-				-1, -2, 16, -2, -1,
-				0, -1, -2, -1, 0,
-				0, 0, -1, 0, 0
-			]
+				-1,	-1,	-1,	-1,	-1,
+				-1,	2,	2,	2,	-1,
+				-1,	2,	0,	2,	-1,
+				-1,	2,	2,	2,	-1,
+				-1,	-1,	-1,	-1,	-1
+			],
+			true
 		),
+		arr = newData.data,
+		arrCount = arr.length,
+		i, r, g, b, v;
+
+	for (i = 0; i < arrCount; i += 4) {
+		r = arr[i];
+		g = arr[i+1];
+		b = arr[i+2];
+
+		v = (r + g + b) / 3;
+		v *= 1.1;
+
+		v = v >= data.value ? 255 : 0;
+
+		arr[i] = arr[i+1] = arr[i+2] = v;
+	}
+
+	// Apply the filter and then put the new pixel data
+	ctx.putImageData(
+		newData,
 		0,
 		0
 	);
