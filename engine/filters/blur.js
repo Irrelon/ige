1d8127b1d435589523faa64fc8e217506897ffeb
Rob Evans <rob@irrelon.com>
2012-11-17 08:54:09 +0000

    Performance optimised

--- engine/filters/blur.js
+++ engine/filters/blur.js
@@ -3,28 +3,36 @@ IgeFilters.blur = function (canvas, ctx, originalImage, texture, data) {
 	ctx.drawImage(originalImage, 0, 0);
 
 	var strength = 1,
-		loop;
+		loop,
+		oneNinth = 1 / 9,
+		pixelData;
+
+	pixelData = ctx.getImageData(
+		0,
+		0,
+		canvas.width,
+		canvas.height
+	);
 
 	if (data && data.value) {
 		strength = data.value;
 	}
 
 	for (loop = 0; loop < strength; loop++) {
-		// Apply the filter and then put the new pixel data
-		ctx.putImageData(
-			IgeFilters._convolute(
-				ctx.getImageData(
-					0,
-					0,
-					canvas.width,
-					canvas.height
-				),
-				[ 1/9, 1/9, 1/9,
-					1/9, 1/9, 1/9,
-					1/9, 1/9, 1/9 ]
-			),
-			0,
-			0
+		pixelData = IgeFilters._convolute(
+			pixelData,
+			[
+				oneNinth, oneNinth, oneNinth,
+				oneNinth, oneNinth, oneNinth,
+				oneNinth, oneNinth, oneNinth
+			]
 		);
 	}
+
+	// Put the new pixel data
+	ctx.putImageData(
+		pixelData,
+		0,
+		0
+	);
 };
