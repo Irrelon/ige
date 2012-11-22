10aa8208e151aa11f5dfa7a4ad024eb0d52044bf
Rob Evans <coolbloke1324@gmail.com>
2012-11-22 00:44:38 +0000

    Chrome extension scenegraph now working

--- tools/chrome-extension/panel.js
+++ tools/chrome-extension/panel.js
@@ -1,11 +1,58 @@
+var selectedItem;
+
 function addToSG(item) {
 	var elem = document.createElement('li'),
 		arr,
 		arrCount,
-		i;
+		i,
+		mouseOver,
+		mouseOut,
+		mouseUp;
+
+	mouseOver = function (event) {
+		if (!selectedItem) {
+			event.stopPropagation();
+			//document.getElementById('igeConsole').innerHTML += "ige.$('vp1').drawBoundsLimitId('" + this.id + "');" + '<br />';
+			chrome.devtools.inspectedWindow.eval("ige.$('vp1').drawBoundsLimitId('" + this.id + "');", function (result, isException) {
+
+			});
+		}
+	};
+
+	mouseOut = function (event) {
+		if (!selectedItem) {
+			event.stopPropagation();
+			//document.getElementById('igeConsole').innerHTML += "ige.$('vp1').drawBoundsLimitId('');" + '<br />';
+			chrome.devtools.inspectedWindow.eval("ige.$('vp1').drawBoundsLimitId('');", function (result, isException) {
+
+			});
+		}
+	};
+
+	mouseUp = function () {
+		var arr = document.getElementsByClassName('sgItem'),
+			arrCount = arr.length,
+			i;
+
+		for (i = 0; i < arrCount; i++) {
+			arr[i].className = 'sgItem';
+		}
+
+		event.stopPropagation();
+		selectedItem = this.id;
+		this.className = 'sgItem selected';
+		chrome.devtools.inspectedWindow.eval("ige.$('vp1').drawBoundsLimitId('" + this.id + "');", function (result, isException) {
+
+		});
+	};
+
+	//elem.addEventListener('mouseover', mouseOver, false);
+	//elem.addEventListener('mouseout', mouseOut, false);
+	elem.addEventListener('mouseup', mouseUp, false);
 
 	elem.id = item.id;
 	elem.innerHTML = item.text;
+	elem.className = 'sgItem';
 
 	if (!parent) {
 		parent = 'sceneGraph';
@@ -28,6 +75,7 @@ function addToSG(item) {
 	}
 }
 
+chrome.devtools.inspectedWindow.eval("ige.$('vp1').drawBounds(true).drawBoundsData(true);");
 chrome.devtools.inspectedWindow.eval('ige.getSceneGraphData(ige, true);', function (result, isException) {
 	addToSG(result);
 });
