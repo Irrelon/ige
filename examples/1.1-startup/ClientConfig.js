a0f7800311789357fa42863eff84bb0e651d062f
Rob Evans <rob@irrelon.com>
2012-12-13 17:16:55 +0000

    Added an external library to the startup example to show how to use external libraries

--- examples/1.1-startup/ClientConfig.js
+++ examples/1.1-startup/ClientConfig.js
@@ -1,5 +1,9 @@
 var igeClientConfig = {
 	include: [
+		/* Some external libraries if you want */
+		// jQuery is not require for the engine to run, this is just an example
+		'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js',
+
 		/* Your custom game JS scripts */
 		'./gameClasses/ClientNetworkEvents.js',
 		'./gameClasses/Rotator.js',
