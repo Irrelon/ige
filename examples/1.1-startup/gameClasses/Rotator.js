57cf485c5211701d515f125017feab55947ed07a
Rob Evans <rob@irrelon.com>
2012-12-13 18:47:11 +0000

    Updated rotator class to accept speed value

--- examples/1.1-startup/gameClasses/Rotator.js
+++ examples/1.1-startup/gameClasses/Rotator.js
@@ -1,13 +1,23 @@
 var Rotator = IgeEntity.extend({
 	classId:'Rotator',
 
+	init: function (speed) {
+		this._super();
+
+		if (speed !== undefined) {
+			this._rSpeed = speed;
+		} else {
+			this._rSpeed = 0;
+		}
+	},
+
 	/**
 	 * Called every frame by the engine when this entity is mounted to the scenegraph.
 	 * @param ctx The canvas context to render to.
 	 */
 	tick: function (ctx) {
 		// Rotate this entity by 0.1 degrees.
-		this.rotateBy(0, 0, (0.1 * ige._tickDelta) * Math.PI / 180);
+		this.rotateBy(0, 0, (this._rSpeed * ige._tickDelta) * Math.PI / 180);
 
 		// Call the IgeEntity (super-class) tick() method
 		this._super(ctx);
