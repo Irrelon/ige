e6e9f1dc426219f8884f4960cd7c7c8116c9b0e4
Rob Evans <rob@irrelon.com>
2013-01-05 15:50:58 +0000

    Added assets, changed paths

--- examples/6.1-animation/gameClasses/Character.js
+++ examples/6.1-animation/gameClasses/Character.js
@@ -12,7 +12,7 @@ var Character = IgeEntity.extend({
 			.depth(1);
 
 		// Load the character texture file
-		this._characterTexture = new IgeCellSheet('../assets/textures/sprites/vx_chara02_c.png', 12, 8);
+		this._characterTexture = new IgeCellSheet('./assets/textures/sprites/vx_chara02_c.png', 12, 8);
 
 		// Wait for the texture to load
 		this._characterTexture.on('loaded', function () {
