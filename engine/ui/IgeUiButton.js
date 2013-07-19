16ad30acbd5a1ec662b271048cd47935b531204d
Rob Evans <rob@irrelon.com>
2013-07-19 14:39:03 +0100

    Updated IgeUiButton to allow text to be rendered on top of it

--- engine/ui/IgeUiButton.js
+++ engine/ui/IgeUiButton.js
@@ -6,6 +6,22 @@ var IgeUiButton = IgeUiEntity.extend({
 		if (this._mouseUp) { this._mouseUp(); }
 
 		return this;
+	},
+	
+	tick: function (ctx) {
+		var uiData = this.data('ui');
+		if (uiData) {
+			// Draw text centered
+			if (uiData['text']) {
+				ctx.font = uiData['text'].font || "normal 12px Verdana";
+				ctx.textAlign = uiData['text'].align || 'center';
+				ctx.textBaseline = uiData['text'].baseline || 'middle';
+				ctx.fillStyle = uiData['text'].color || '#ffffff';
+				ctx.fillText(uiData['text'].value, 0, 0);
+			}
+		}
+		
+		IgeUiEntity.prototype.tick.call(this, ctx);
 	}
 });
 
