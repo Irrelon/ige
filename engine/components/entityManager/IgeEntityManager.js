c173843a7397449a74184f7a6f345c0f6cb78a68
Rob Evans <rob@irrelon.com>
2013-09-13 21:25:49 +0100

    Doc comments, logic updates to mark dynamic entities via a new managed mode

--- engine/components/entityManager/IgeEntityManager.js
+++ engine/components/entityManager/IgeEntityManager.js
@@ -19,7 +19,13 @@ var IgeEntityManager = IgeEventingClass.extend({
 		// Set a method (behaviour) that will be called on every update
 		entity.addBehaviour('path', this._updateBehaviour, false);
 	},
-	
+
+	/**
+	 * Called each update frame from the component parent and calls various private
+	 * methods to ensure that entities that should be mounted are mounted and those
+	 * that are to be unmounted are unmounted.
+	 * @private
+	 */
 	_updateBehaviour: function () {
 		// Get our instance back
 		var self = this.entityManager;
@@ -30,7 +36,13 @@ var IgeEntityManager = IgeEventingClass.extend({
 		self._processMountQueue();
 		self._processUnMountQueue();
 	},
-	
+
+	/**
+	 * Checks all the mounted entities to our component parent are still supposed
+	 * to be in the scenegraph and if not, adds them to the unmount queue. Also
+	 * marks any entities that are non-managed but also off-screen as inView = false.
+	 * @private
+	 */
 	_updateOrphans: function () {
 		// Loop our child entities and check for new orphans (entities that are
 		// set to managed and are not intersecting the bounds of any active viewport
@@ -47,9 +59,20 @@ var IgeEntityManager = IgeEventingClass.extend({
 				if (item.aabb) {
 					// Check the entity to see if its bounds are "inside" the
 					// viewport's visible area
-					if (!vpViewArea.rectIntersect(item.aabb(true))) {
-						// The entity is not inside the viewport visible area
-						this._unMountQueue.push(item);
+					if (!vpViewArea.rectIntersect(item.aabb())) {
+						// Check for managed mode 1 (static entities that can be unmounted)
+						// or managed mode 2 (dynamic and should just be marked as inView = false)
+						if (item._managed === 1) {
+							// The entity is not inside the viewport visible area
+							// and is managed mode 1 (static) so unmount it
+							this._unMountQueue.push(item);
+						} else if (item._managed === 2) {
+							// The entity is dynamic so mark is as inView = false
+							item._inView = false;
+						}
+					} else if (item._managed === 2) {
+						// The entity is dynamic so mark is as inView = true
+						item._inView = true;
 					}
 				} else {
 					this._unMountQueue.push(item);
@@ -58,6 +81,11 @@ var IgeEntityManager = IgeEventingClass.extend({
 		}
 	},
 	
+	/**
+	 * Checks all the un-mounted entities for our component parent to see if they are
+	 * now inside the visible area of a viewport and if so, queues them for re-mounting.
+	 * @private
+	 */
 	_updateChildren: function () {
 		// Loop our child entities and check for new orphans (entities that are
 		// set to managed and are not intersecting the bounds of any active viewport
@@ -75,14 +103,21 @@ var IgeEntityManager = IgeEventingClass.extend({
 					// Check the entity to see if its bounds are "inside" the
 					// viewport's visible area
 					if (vpViewArea.rectIntersect(item.aabb())) {
-						// The entity is not inside the viewport visible area
+						// The entity is inside the viewport visible area
 						this._mountQueue.push(item);
+						
+						// Mark the entity as inView = true
+						item._inView = true;
 					}
 				}
 			}
 		}
 	},
-	
+
+	/**
+	 * Loops any entities queued for mounting and mounts them.
+	 * @private
+	 */
 	_processMountQueue: function () {
 		var arr = this._mountQueue,
 			arrCount = arr.length,
@@ -97,7 +132,11 @@ var IgeEntityManager = IgeEventingClass.extend({
 		
 		this._mountQueue = [];
 	},
-	
+
+	/**
+	 * Loops any entities queued for un-mounting and un-mounts them.
+	 * @private
+	 */
 	_processUnMountQueue: function () {
 		var arr = this._unMountQueue,
 			arrCount = arr.length,
