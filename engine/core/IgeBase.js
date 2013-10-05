660fb0496b8820a692a6f8865cd77f5d773ad63a
Rob Evans <rob@irrelon.com>
2013-10-05 15:28:03 +0100

    Version bump

--- engine/core/IgeBase.js
+++ engine/core/IgeBase.js
@@ -9,7 +9,7 @@ ige = null;
 // For example, to tag version 1.1.2 on 25th April 2013
 // as the third revision of the day:
 // v1.1.2@2013-04-25.003
-igeVersion = 'v1.2.2@13-10-05.001';
+igeVersion = 'v1.2.2@13-10-05.002';
 
 // Define the global storage object for classes
 igeClassStore = {};
