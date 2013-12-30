9cf52e7bb4ca53d9682cd45172d5f027a8f6b6d7
Rob Evans <rob@irrelon.com>
2013-12-30 23:16:27 +0000

    Updated playerComponent in example

--- examples/9.5-goto-path-finder/gameClasses/PlayerComponent.js
+++ examples/9.5-goto-path-finder/gameClasses/PlayerComponent.js
@@ -16,7 +16,7 @@ var PlayerComponent = IgeClass.extend({
 		this._options = options;
 
 		// Listen for the mouse up event
-		ige.input.on('mouseUp', function (event, x, y, button) { self._mouseUp(event, x, y, button); });
+		ige.input.on('mouseUp', function () { self._mouseUp(); });
 	},
 
 	/**
@@ -24,39 +24,14 @@ var PlayerComponent = IgeClass.extend({
 	 * @param event
 	 * @private
 	 */
-	_mouseUp: function (event, x, y, button) {
+	_mouseUp: function () {
 		// Get the tile co-ordinates that the mouse is currently over
-		var endTile = ige.$('tileMap1').mouseToTile(),
-			currentPosition = this._entity._translate,
-			startTile,
-			newPath,
-			endPoint = this._entity.path.endPoint();
-
-		// Check if we have a current path, if so, add to it
-		if (endPoint) {
-			// Use the end point of the existing path as the
-			// start point of the new path
-			startTile = endPoint;
-		} else {
-			// Calculate which tile our character is currently "over"
-			if (this._entity._parent.isometricMounts()) {
-				startTile = this._entity._parent.pointToTile(currentPosition.toIso());
-			} else {
-				startTile = this._entity._parent.pointToTile(currentPosition);
-			}
-		}
-
-		// Create a path from the current position to the target tile
-		newPath = ige.client.pathFinder.generate(ige.client.tileMap1, startTile, endTile, function (tileData, tileX, tileY) {
-			// If the map tile data is set to 1, don't allow a path along it
-			return tileData !== 1;
-		}, true, true, true);
+		var endTile = ige.$('tileMap1').mouseToTile();
 
 		// Tell the entity to start pathing along the new path
-		this._entity
-			//.path.clear()
-			.path.add(newPath)
-			.path.start();
+		this._entity.path
+			.add(endTile.x, endTile.y, 0)
+			.start();
 	}
 });
 
