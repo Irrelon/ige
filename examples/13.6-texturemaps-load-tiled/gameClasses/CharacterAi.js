1caba0228b9267e42ef3db998cb1a7ef0f4c7d32
Rob Evans <rob@irrelon.com>
2013-12-31 17:01:46 +0000

    Fixed example issues

--- examples/13.6-texturemaps-load-tiled/gameClasses/CharacterAi.js
+++ examples/13.6-texturemaps-load-tiled/gameClasses/CharacterAi.js
@@ -8,27 +8,34 @@ var CharacterAi = Character.extend({
 
 		Character.prototype.init.call(this);
 
-		this.pathFinder = pathFinder;
 		this.collisionMap = collisionMap;
+		this.tileChecker = function (tileData, tileX, tileY) {
+			// If the map tile data is set, don't path along it
+			return !tileData;
+		};
 
 		// Choose a random character type
 		this.setType(Math.random() * 8 | 0);
 
 		// Add pathing capabilities
-		this.addComponent(IgePathComponent)
-			.path.drawPath(false); // Enable drawing the current path
+		this.addComponent(IgePathComponent).path
+			.speed(2)
+			.finder(pathFinder)
+			.tileMap(this.collisionMap)
+			.tileChecker(this.tileChecker)
+			.drawPath(true); // Enable drawing the current path
 
 		// Hook the path events
 		newPathMethod = function () {
 			self.newPath();
 		};
 
-		this.path.on('traversalComplete', newPathMethod);
+		this.path.on('pathComplete', newPathMethod);
 
 		// Hook when we get mounted
 		this.on('mounted', function (parent) {
 			// Start the first path!
-			self.pathNextUpdate = true;
+			self.pathNextTick = true;
 		});
 	},
 
@@ -36,52 +43,40 @@ var CharacterAi = Character.extend({
 		var self = this,
 			currentTile,
 			destTileX,
-			destTileY,
-			destTile,
-			path = [],
-			tileChecker = function (tileData, tileX, tileY) {
-				// If the map tile data is set, don't path along it
-				return !tileData;
-			};
+			destTileY;
 
 		// Calculate which tile our character is currently "over"
-		currentTile = this._parent.pointToTile(this._translate.toIso());
+		currentTile = this._parent.pointToTile(this._translate);
 
 		// Pick a random destination tile
 		destTileX = currentTile.x + ((Math.random() * 20 | 0) - 10);
 		destTileY = currentTile.y + ((Math.random() * 20 | 0) - 10);
 
 		if (destTileX < 0 || destTileY < 0) {
-			self.pathNextUpdate = true;
+			self.pathNextTick = true;
 			return;
 		}
 
-		if (!this.collisionMap.map._mapData[destTileY] || !tileChecker(this.collisionMap.map._mapData[destTileY][destTileX])) {
-			self.pathNextUpdate = true;
+		if (!this.collisionMap.map._mapData[destTileY] || !this.tileChecker(this.collisionMap.map._mapData[destTileY][destTileX])) {
+			self.pathNextTick = true;
 			return;
 		}
 
-		destTile = new IgePoint3d(destTileX, destTileY, 0);
-		path = self.pathFinder.generate(self.collisionMap, currentTile, destTile, tileChecker, true, false);
-
-		if (path.length) {
 			// Assign the path to the player and start it
-			self.path.clear()
-				.path.add(path)
-				.path.start();
+		self.path
+			.clear()
+			.add(destTileX, destTileY, 0)
+			.start();
 
-			self.pathNextUpdate = false;
-		} else {
-			self.pathNextUpdate = true;
-		}
+		self.pathNextTick = false;
 	},
 
-	update: function (ctx, tickDelta) {
-		if (this.pathNextUpdate) {
+	tick: function (ctx) {
+		if (this.pathNextTick) {
 			this.newPath();
 		}
 
-		Character.prototype.update.call(this, ctx, tickDelta);
+		Character.prototype.tick.call(this, ctx);
 	}
 });
 
