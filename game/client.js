83a3d0956a9917e2b83228ac806f00d953dc3047
Rob Evans <rob@irrelon.com>
2014-04-02 18:01:05 +0100

    Revert "cooode"
    This reverts commit d93737451437070a5d045ca947f04cfcca0b3ecf.

--- game/client.js
+++ game/client.js
@@ -4,13 +4,9 @@ var Client = IgeClass.extend({
 	init: function () {
 		//ige.timeScale(0.1);
 		ige.showStats(1);
-        ige.globalSmoothing(true);
 
 		// Load our textures
 		var self = this;
-        self.networkStarted = false;
-
-        this.implement(ClientNetworkEvents);
 
 		// Enable networking
 		ige.addComponent(IgeNetIoComponent);
@@ -20,8 +16,7 @@ var Client = IgeClass.extend({
 
 		// Load the textures we want to use
 		this.textures = {
-			logo: new IgeTexture('./assets/logo.png'),
-            loginButton: new IgeTexture('./assets/loginButton.png')
+			ship: new IgeTexture('./assets/PlayerTexture.js')
 		};
 
 		ige.on('texturesLoaded', function () {
@@ -29,153 +24,29 @@ var Client = IgeClass.extend({
 			ige.start(function (success) {
 				// Check if the engine started successfully
 				if (success) {
-
-                    // apply all the "css" biz
-                    self.setUiStyles();
-
-                    self.baseScene = new IgeScene2d()
-                        .id('baseScene');
-
-                    self.vp1 = new IgeViewport()
-                        .id('vp1')
-                        .autoSize(true)
-                        .scene(self.baseScene)
-                        .drawBounds(false)
-                        .mount(ige);
-
-                    self.uiScene = new IgeScene2d()
-                        .id('uiScene')
-                        .depth(1)
-                        .ignoreCamera(true)
-                        .mount(self.baseScene);
-
-                    self.mainDiv = new IgeUiElement()
-                        .id('mainDiv')
-                        .styleClass('main')
-                        .mount(self.uiScene);
-
-                    self.loginDiv = new IgeUiElement()
-                        .id('loginDiv')
-                        .styleClass('loginDiv')
-                        .mount(self.mainDiv);
-
-                    self.tbUsername = new IgeUiTextBox()
-                        .id('tbUsername')
-                        .value('')
-                        .placeHolder('Username')
-                        .placeHolderColor('#989898')
-                        .mount(self.loginDiv);
-
-                    self.tbPassword = new IgeUiTextBox()
-                        .id('tbPassword')
-                        .value('')
-                        .mask('*')
-                        .placeHolder('Password')
-                        .placeHolderColor('#989898')
-                        .mount(self.loginDiv);
-
-                    self.btnLogin = new IgeUiElement()
-                        .id('btnLogin')
-                        .texture(self.textures.loginButton)
-                        .dimensionsFromTexture()
-                        .mouseUp(function(){
-                            //ToDo
-                            var username = self.tbUsername.value(),
-                                password = self.tbPassword.value();
-                            if (username.length > 2 && password.length > 2) {
-                                ige.network.start('http://localhost:2000', function () {
-
-
-                                    ige.network.define('onDbTest', self._onDbTest);
-
-                                    self.networkStarted = true;
-					            });
-
-                                if (self.networkStarted) {
-                                    console.log('networking started, trying db connection...');
-                                    ige.network.send('onDbTest', { username: username, password: password });
-                                    console.log('sent!');
-                                }
-                            }
-                        })
-                        .mount(self.loginDiv);
-
-                    self.logo = new IgeUiElement()
-                        .id('logo')
-                        .texture(self.textures.logo)
-                        .dimensionsFromTexture(30) // argument = % of original size
-                        .mount(self.loginDiv);
-
 					// Start the networking (you can do this elsewhere if it
 					// makes sense to connect to the server later on rather
 					// than before the scene etc are created... maybe you want
 					// a splash screen or a menu first? Then connect after you've
 					// got a username or something?
-//					ige.network.start('http://localhost:2000', function () {
-//						// Setup the network stream handler
-//					});
+					ige.network.start('http://localhost:2000', function () {
+						// Setup the network stream handler
+						ige.network.addComponent(IgeStreamComponent)
+							.stream.renderLatency(80) // Render the simulation 160 milliseconds in the past
+							// Create a listener that will fire whenever an entity
+							// is created because of the incoming stream data
+							.stream.on('entityCreated', function (entity) {
+								self.log('Stream entity created with ID: ' + entity.id());
+
+							});
+						
+						// Load the base scene data
+						ige.addGraph('IgeBaseScene');
+					});
 				}
 			});
 		});
-	},
-
-    setUiStyles: function() {
-
-        ige.ui.style('IgeUiLabel', {
-            'font': '14px Open Sans',
-            'color': '#333333'
-        });
-
-        ige.ui.style('IgeUiTextBox', {
-            'backgroundColor': '#ffffff',
-            'borderColor': '#212121',
-            'borderWidth': 1,
-            'borderRadius': 4,
-            'bottom': null,
-            'right': null,
-            'width': 200,
-            'height': 30,
-//            'left': '50%',
-            'font': '12px Open Sans',
-            'color': '#000000'
-        });
-
-        ige.ui.style('.main', {
-            'backgroundColor': '#dddddd',
-            'top': 0,
-            'left': 0,
-            'width': '100%',
-            'height': '100%'
-        });
-
-        ige.ui.style('.loginDiv', {
-            'backgroundColor': '#eee',
-            'width': 400,
-            'height': 250,
-            'borderWidth': 1,
-            'borderColor': '#212121'
-        });
-
-        ige.ui.style('#btnLogin', {
-            'bottom': 20,
-            'right': '50%'
-        });
-
-        ige.ui.style('#logo', {
-            'top': 10,
-//            'left': 0
-        });
-
-        ige.ui.style('#tbUsername', {
-            'top': '42%'
-        });
-
-        ige.ui.style('#tbPassword', {
-            'top': '58%'
-        });
-
-
-    }
+	}
 });
 
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = Client; }
--- game/gameClasses/ClientNetworkEvents.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Jimmy on 2014-03-30.
- */
-var ClientNetworkEvents = {
-
-    _onDbTest: function(data) {
-        console.log("DB data received from server!", data);
-    }
-}
