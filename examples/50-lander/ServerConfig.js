fe413cbb879f86733dfbe27e03f18fffcaf10648
irrelon <rob@irrelon.com>
2017-07-04 19:45:47 +0100

    Cleanup

--- examples/50-lander/ServerConfig.js
+++ examples/50-lander/ServerConfig.js
@@ -1,7 +1,8 @@
 var igeConfig = {
-	include: [
-		{name: 'ServerNetworkEvents', path: './gameClasses/ServerNetworkEvents'}
-	]
+	include: [{
+		name: 'ServerNetworkEvents',
+		path: './gameClasses/ServerNetworkEvents'
+	}]
 };
 
 if (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined') { module.exports = igeConfig; }
