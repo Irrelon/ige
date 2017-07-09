d3d715da51afd1ada41eeb7f410280f4446300b6
irrelon <rob@irrelon.com>
2017-07-09 23:45:24 +0100

    Added $game module as a shared "root scope" object

---
+++ engine/core/$game.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var appCore = require('irrelon-appcore');
+
+appCore.module('$game', function () {
+	// Return a blank object that can be shared around the application
+	return {};
+});
