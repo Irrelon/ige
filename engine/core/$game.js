4eb1c43406682c30c32d7e0e2c5c6efd06fa4436
irrelon <rob@irrelon.com>
2017-07-17 20:37:54 +0100

    Added isClient and isServer to $game class

--- engine/core/$game.js
+++ engine/core/$game.js
@@ -4,7 +4,9 @@ var appCore = require('irrelon-appcore');
 
 appCore.module('$game', function () {
 	var $game = function () {
-		
+		// Determine the environment we are executing in
+		this.isServer = (typeof(module) !== 'undefined' && typeof(module.exports) !== 'undefined' && typeof window === 'undefined');
+		this.isClient = !this.isServer;
 	};
 	
 	return new $game();
