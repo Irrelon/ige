9c42c5b9389dc0b446c63358d06d0cb66943fce9
Rob Evans <rob@irrelon.com>
2024-01-02 14:19:20 +0000

    Updated the index file generator so we can have automatic index.ts file creation rather than having to manually export everything all the time

--- update-indexes.js
+++ update-indexes.js
@@ -5,32 +5,36 @@ const yargs = require("yargs");
 const fs = require("fs");
 const path = require("path");
 
-const argv = yargs.default("src", "src/**/").argv;
+const argv = yargs.default("src", "src/**").argv;
 
 function createIndexFile (folder) {
 	console.info(`index: ${folder}`);
-	const indexFile = `${folder}index.ts`;
+	const indexFile = `${folder}/index.ts`;
 	if (fs.existsSync(indexFile)) {
-		fs.unlinkSync(indexFile);
-	}
-	const tsFiles = glob.sync(`${folder}*.ts`);
-	if (tsFiles.length == 0) {
 		return;
 	}
+
+	const tsFiles = glob.sync(`${folder}/*.ts`, { ignore: ["**/node_modules/**", "**/*.old*"] });
+	if (tsFiles.length === 0) {
+		console.info(`No files to export: ${folder}/*.ts`);
+		return;
+	}
+
 	let code = "";
 	for (const tsFile of tsFiles) {
 		const baseName = path.basename(tsFile, ".ts");
 		code += `export * from "./${baseName}";\n`;
 	}
+
 	fs.writeFileSync(indexFile, code);
 }
 
-const files = glob.sync(argv.src, { ignore: ["**/node_modules/**"] });
+const files = glob.sync(argv.src, { ignore: ["**/node_modules/**", "**/*.old*"] });
 for (const folder of files) {
 	console.log(`Scanning ${folder}`);
-	const rcFile = folder + ".index.json";
-	if (fs.existsSync(rcFile) == false) {
-		continue;
-	}
+	//const rcFile = folder + ".index.json";
+	// if (fs.existsSync(rcFile) == false) {
+	// 	continue;
+	// }
 	createIndexFile(folder);
 }
