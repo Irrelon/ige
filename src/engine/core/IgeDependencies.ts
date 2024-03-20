0c5bdeb8b94bd956caf18f8b160f4cd929c3d905
Rob Evans <rob@irrelon.com>
2024-03-20 17:10:03 +0000

    Quotes around dependency name

--- src/engine/core/IgeDependencies.ts
+++ src/engine/core/IgeDependencies.ts
@@ -13,7 +13,7 @@ export class IgeDependencies {
 				this._onDependencySatisfied(dependencyName);
 			})
 			.catch((err) => {
-				throw new Error(`Dependency ${dependencyName} threw an error: ${err}`);
+				throw new Error(`Dependency "${dependencyName}" threw an error: ${err}`);
 			});
 	}
 
