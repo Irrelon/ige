9dddb147afe95630bcfbf313170c3ea9f8dddf2d
Rob Evans <rob@irrelon.com>
2024-07-19 12:28:50 +0100

    Added the renderer to exports

--- src/export/exports.ts
+++ src/export/exports.ts
@@ -148,6 +148,7 @@ export * from "../engine/core/IgeEntity";
 export * from "../engine/core/IgeCamera";
 export * from "../engine/core/IgeBaseRenderer";
 export * from "../engine/core/IgeCanvas2dRenderer";
+export * from "../engine/core/IgeThreeJsRenderer";
 export * from "../engine/core/IgeEngine";
 export * from "../engine/network/chat/IgeChatComponent";
 export * from "../engine/core/IgeUiEntity";
