ffdc036c0f858e0fba6c67d839834dd71181446d
Rob Evans <rob@irrelon.com>
2024-07-19 12:27:46 +0100

    Updated smart texture to typescript

--- src/assets/textures/smartTextures/simpleBox.ts
+++ src/assets/textures/smartTextures/simpleBox.ts
@@ -1,7 +1,7 @@
 import type { IgeEntity } from "@/engine/core/IgeEntity";
 import type { IgeCanvasRenderingContext2d } from "@/types/IgeCanvasRenderingContext2d";
 
-const image = {
+export const image = {
 	render: function (ctx: IgeCanvasRenderingContext2d, entity: IgeEntity) {
 		ctx.fillStyle = "#ffffff";
 		ctx.fillRect(-entity._bounds2d.x2, -entity._bounds2d.y2, entity._bounds2d.x, entity._bounds2d.y);
