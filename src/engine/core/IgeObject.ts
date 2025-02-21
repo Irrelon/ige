42f5e1d971151681a0a020a2d5b3a0b907d54e3b
Rob Evans <rob@irrelon.com>
2025-02-21 11:55:20 +0000

    Default the texture cell of an object to 1

--- src/engine/core/IgeObject.ts
+++ src/engine/core/IgeObject.ts
@@ -100,7 +100,7 @@ export class IgeObject extends IgeEventingClass implements IgeCanRegisterById, I
 	_renderPos: { x: number; y: number };
 	_computedOpacity: number;
 	_opacity: number;
-	_cell: number | null = 1;
+	_cell: number = 1;
 	_deathTime?: number;
 	_bornTime: number = 0;
 	_translate: IgePoint3d;
